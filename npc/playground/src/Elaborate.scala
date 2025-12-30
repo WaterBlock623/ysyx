4b4e2643473130216e81492ed4a1b2a29a31274d
tracer-ysyx <tracer@ysyx.org>
2025-12-30 15:18:29 +0800

    > generate verilog ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:18:29 up  6:30,  1 user,  load average: 0.85, 0.40, 0.33

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -8,5 +8,5 @@ object Elaborate extends App {
       "locationInfoStyle=wrapInAtSquareBracket"
     ).reduce(_ + "," + _)
   )
-  circt.stage.ChiselStage.emitSystemVerilogFile(new dle.exp3.top(), args, firtoolOptions)
+  circt.stage.ChiselStage.emitSystemVerilogFile(new dle.exp3.Top(), args, firtoolOptions)
 }
