95dcb79110ca3aa912b860047fc633cd955821ee
tracer-ysyx <tracer@ysyx.org>
2025-12-30 12:16:47 +0800

    > generate verilog ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:16:47 up  3:21,  1 user,  load average: 0.01, 0.03, 0.06

---
+++ npc/playground/src/Light.scala
@@ -0,0 +1,15 @@
+package light
+import chisel3._
+
+class Light(maxCnt: Int = 5000000) extends Module {
+  val io = IO(new Bundle {
+    val led = Output(UInt(16.W))
+  })
+  val cntReg = RegInit(0.U(32.W))
+  cntReg := Mux(cntReg === maxCnt.U, 0.U, cntReg + 1.U)
+  val ledReg = RegInit(1.U(16.W))
+  ledReg := Mux(cntReg === maxCnt.U, ledReg(14, 0) ## ledReg(15), ledReg)
+  io.led := ledReg
+}
+
+
--- npc_bak/csrc/sim_main.cpp
+++ npc_bak/csrc/sim_main.cpp
@@ -1,9 +1,12 @@
+#include "Vtop.h"
 #include "verilated.h"
 #include "verilated_fst_c.h"
 #include <nvboard.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+
+#define TO_STR(a) #a
 
 void nvboard_bind_all_pins(TOP_NAME* top);
 
@@ -15,18 +18,18 @@ void sim_init(int argc, char** argv)
 {
     contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
-    top = new TOP_NAME{contextp};
+    top = new Vtop{contextp};
 	nvboard_bind_all_pins(top);
 	nvboard_init();
-//	Verilated::traceEverOn(true);
-//	tfp = new VerilatedFstC;
-//	top->trace(tfp, 99);
-//	tfp->open("./build/obj_dir/wave/sim.fst");
+	Verilated::traceEverOn(true);
+	tfp = new VerilatedFstC;
+	top->trace(tfp, 99);
+	tfp->open("./build/obj_dir/wave/sim.fst");
 }
 
 void sim_close(void)
 {
-//	tfp->close();
+	tfp->close();
     delete top;
     delete contextp;
 	nvboard_quit();
@@ -34,14 +37,14 @@ void sim_close(void)
 
 void single_cycle(void)
 {
-	top->clock = 0; top->eval();
-	top->clock = 1; top->eval();
+	top->clk = 0; top->eval();
+	top->clk = 1; top->eval();
 }
 
 void reset(int n) {
-	top->reset = 1;
+	top->rst = 1;
 	while (n-- > 0) single_cycle();
-	top->reset = 0;
+	top->rst = 0;
 }
 
 int main(int argc, char** argv) {
@@ -49,10 +52,10 @@ int main(int argc, char** argv) {
 	sim_init(argc, argv);
 	reset(10);
     while ((contextp->time() < sim_time | sim_time == -1) && !contextp->gotFinish()) {
-//		contextp->timeInc(1);
+		contextp->timeInc(1);
 		nvboard_update();
 		single_cycle();
-//		tfp->dump(contextp->time());
+		tfp->dump(contextp->time());
 	}
 	sim_close();
     return 0;
