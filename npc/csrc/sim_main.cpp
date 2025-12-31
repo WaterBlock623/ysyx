e9616b397953423f74786a455fa06e00ba31bc18
tracer-ysyx <tracer@ysyx.org>
2025-12-31 17:10:31 +0800

    > sim RTL ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:10:31 up  7:39,  1 user,  load average: 0.08, 0.07, 0.09

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -50,7 +50,7 @@ void reset(int n) {
 }
 
 int main(int argc, char** argv) {
-	int sim_time = -1;
+	int sim_time = 50000000;
 	sim_init(argc, argv);
 	reset(10);
     while ((contextp->time() < sim_time | sim_time == -1) && !contextp->gotFinish()) {
