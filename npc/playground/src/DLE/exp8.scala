cff814c3321e38edf0b57a6a5acae8adb31bf3fe
tracer-ysyx <tracer@ysyx.org>
2026-01-01 15:39:50 +0800

    > generate verilog ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:39:50 up  5:55,  1 user,  load average: 1.43, 0.43, 0.14

---
+++ npc/playground/src/DLE/exp8.scala
@@ -0,0 +1,75 @@
+package dle.exp8
+
+import chisel3._
+import chisel3.util._
+import chisel3.util.experimental.loadMemoryFromFileInline
+
+class VgaSignal() extends Bundle {
+    val hs = Output(Bool())
+    val vs = Output(Bool())
+    val blankN = Output(Bool())
+    val rgbOut = Output(UInt(24.W))
+}
+
+case class VgaCfg(
+  hFrontPorchWidth: Int = 16,
+  hSyncWidth: Int = 96,
+  hBackPorchWidth: Int = 48,
+  hDataWidth: Int = 640,
+
+  vFrontPorchWidth: Int = 10,
+  vSyncWidth: Int = 2,
+  vBackPorchWidth: Int = 33,
+  vDataWidth: Int = 480
+) {
+  val hBlankWidth = hFrontPorchWidth + hSyncWidth + hBackPorchWidth
+  val hTotal: Int = hFrontPorchWidth + hSyncWidth + hBackPorchWidth + hDataWidth
+  val vBlankWidth = vFrontPorchWidth + vSyncWidth + vBackPorchWidth
+  val vTotal: Int = vFrontPorchWidth + vSyncWidth + vBackPorchWidth + vDataWidth
+}
+
+object VgaCfg {
+  implicit val default: VgaCfg = VgaCfg()
+}
+
+class VgaCtrl(implicit val cfg: VgaCfg) extends Module {
+  val io = IO(new Bundle {
+    val rgbIn = Input(UInt(24.W))
+    val vga = new VgaSignal
+    val hAddr = Output(UInt(log2Ceil(cfg.hTotal).W))
+    val vAddr = Output(UInt(log2Ceil(cfg.vTotal).W))
+  })
+
+  val hCntReg = RegInit(0.U(10.W))
+  val vCntReg = RegInit(0.U(10.W))
+  val hMax = cfg.hTotal - 1
+  val vMax = cfg.vTotal - 1
+  hCntReg := Mux(hCntReg === hMax.U, 0.U, hCntReg + 1.U)
+  hCntReg := Mux(hCntReg === hMax.U, Mux(hCntReg === vMax.U, 0.U, hCntReg + 1.U), hCntReg)
+
+  io.vga.rgbOut := io.rgbIn
+  io.vga.hs := RegNext(hCntReg < cfg.hFrontPorchWidth.U || 
+                hCntReg > (cfg.hFrontPorchWidth + cfg.hSyncWidth - 1).U)
+  io.vga.vs := RegNext(hCntReg < cfg.vFrontPorchWidth.U || 
+                vCntReg > (cfg.vFrontPorchWidth + cfg.vSyncWidth - 1).U)
+  val isData = (hCntReg > (cfg.hBlankWidth - 1).U && 
+                    vCntReg > (cfg.vBlankWidth - 1).U)  
+  io.vga.blankN := RegNext(isData)
+  io.hAddr := Mux(isData, hCntReg - cfg.hBlankWidth.U, 0.U)
+  io.vAddr := Mux(isData, vCntReg - cfg.vBlankWidth.U, 0.U)
+}
+
+class Exp8(memFile: String = "/home/waterblock/ysyx-workbench/npc/util/output.hex.txt")(implicit val cfg: VgaCfg) extends Module {
+  val io = IO(new Bundle {
+    val vga = new VgaSignal
+  })
+
+  val mem = SyncReadMem(cfg.vDataWidth, Vec(cfg.hDataWidth, UInt(12.W)))
+  if (memFile.trim().nonEmpty) {
+    loadMemoryFromFileInline(mem, memFile)
+  }
+
+  val vgaCtrl = Module(new VgaCtrl)
+  vgaCtrl.io.vga := io.vga
+  vgaCtrl.io.rgbIn := mem.read(vgaCtrl.io.vAddr)(vgaCtrl.io.hAddr) ## 0.U(12.W)
+}
---
+++ npc/util/imgToHex.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+input_path = "input.jpg"
+output_path = "output.hex.txt"
+size = (640, 480)
+
+img = Image.open(input_path).convert('RGB').resize(size)
+with open(output_path, 'w') as f:
+    for y in range(size[1]):
+        line = ""
+        for x in range(size[0]):
+            r, g, b = img.getpixel((x, y))
+            line += f"{r>>4:x}{g>>4:x}{b>>4:x}"
+        f.write(line + "\n")
