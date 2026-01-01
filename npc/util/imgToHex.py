18c416c200d85f4a21dbf5cc63f727d91a5db9f6
tracer-ysyx <tracer@ysyx.org>
2026-01-01 16:18:13 +0800

    > generate verilog ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:18:13 up  6:34,  1 user,  load average: 0.65, 0.33, 0.36

--- npc/util/imgToHex.py
+++ npc/util/imgToHex.py
@@ -5,10 +5,12 @@ output_path = "output.hex.txt"
 size = (640, 480)
 
 img = Image.open(input_path).convert('RGB').resize(size)
+img.show()
 with open(output_path, 'w') as f:
     for y in range(size[1]):
         line = ""
         for x in range(size[0]):
             r, g, b = img.getpixel((x, y))
-            line += f"{r>>4:x}{g>>4:x}{b>>4:x}"
+            #line += f"{r>>4:x}{g>>4:x}{b>>4:x}"
+            line += f"{r:02x}{g:02x}{b:02x}"
         f.write(line + "\n")
