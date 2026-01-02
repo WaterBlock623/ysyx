fa8c3266f8d9a1ac718845493a58e6d521eea1ed
tracer-ysyx <tracer@ysyx.org>
2026-01-02 16:07:20 +0800

    > generate verilog ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:07:20 up  5:58,  1 user,  load average: 0.42, 0.56, 0.61

--- npc/playground/src/scpu/scpu.scala
+++ npc/playground/src/scpu/scpu.scala
@@ -161,9 +161,7 @@ class Gpr extends Module {
   })
 
   val wEn = io.selSignal.isWriteReg
-  val gpReg = RegInit(0.U.asTypeOf(Vec(4, UInt(8.W))))
-
-  gpReg(io.wAddr) := Mux(wEn, io.wData, gpReg(io.wAddr))
+  val gpReg = RegEnable(io.wData, 0.U.asTypeOf(Vec(4, UInt(8.W))), wEn)
 
   io.rData(0) := gpReg(io.rAddr(0))
   io.rData(1) := gpReg(io.rAddr(1))
