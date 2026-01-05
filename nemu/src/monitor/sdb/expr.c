42b11b2e256c6552891a5cf214217cafdbbfb3b7
tracer-ysyx <tracer@ysyx.org>
2026-01-05 12:35:41 +0800

    > compile NEMU ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:35:41 up  1:53,  1 user,  load average: 0.00, 0.00, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -167,8 +167,10 @@ static bool make_token(char *e) {
          */
         Assert(substr_len > 0, "substr's length is 0");
         printf("nr_token: %d\n", nr_token);
-        struct token *tok_ptr = get_token_ptr();
-        set_token(tok_ptr, i, substr_start, substr_len);
+        if (rules[i].token_type != TK_NOTYPE) {
+          struct token *tok_ptr = get_token_ptr();
+          set_token(tok_ptr, i, substr_start, substr_len);
+        }
         break;
       }
     }
