3fa6724131a3f41cda30b6fce60f077bdbf2756d
Yan Chenrui <17048545@qq.com>
2026-01-06 11:47:09 +0800

    Finish SCpu

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -8,5 +8,5 @@ object Elaborate extends App {
       "locationInfoStyle=wrapInAtSquareBracket"
     ).reduce(_ + "," + _)
   )
-  circt.stage.ChiselStage.emitSystemVerilogFile(new dle.exp8.Exp8(), args, firtoolOptions)
+  circt.stage.ChiselStage.emitSystemVerilogFile(new scpu.scpu.SCpu(), args, firtoolOptions)
 }
