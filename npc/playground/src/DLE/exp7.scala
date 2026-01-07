d86d933d0797d13d7bf5754b7a150ae111b500c3
tracer-ysyx <tracer@ysyx.org>
2026-01-07 18:01:56 +0800

    > generate verilog ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:01:56 up  6:49,  1 user,  load average: 0.64, 0.85, 0.42

--- npc/playground/src/DLE/exp7.scala
+++ npc/playground/src/DLE/exp7.scala
@@ -74,7 +74,7 @@ class ParsePs2() extends Module {
   val dataCntReg = RegInit(0.U(4.W))
   dataCntReg := Mux(negEdge, Mux(dataCntReg === 10.U, 0.U, dataCntReg + 1.U), dataCntReg)
   val dataReg = RegInit(0.U(11.W))
-  dataReg := Mux(negEdge, io.ps2Dat ## dataReg(10, 1), dataReg)
+  dataReg := Mux(negEdge, io.ps2Dat ## dataReg(11, 1), dataReg)
   io.data := dataReg(8, 1)
   io.valid := negEdge & dataCntReg === 10.U
 }
@@ -118,9 +118,10 @@ class Exp7() extends Module {
   val ps = Module(new ParsePs2)
   ps.io.ps2Clk := io.ps2Clk
   ps.io.ps2Dat := io.ps2Dat
-  val dataReg = RegEnable(ps.io.data, 0.U(8.W), ps.io.valid)
+  val validPosEdge = ps.io.valid & ~RegNext(ps.io.valid)
+  val dataReg = RegEnable(ps.io.data, 0.U(8.W), validPosEdge)
   val isKeyDown = ps.io.data =/= "hf0".U && dataReg =/= "hf0".U
-  val keyDownReg = RegEnable(isKeyDown, false.B, ps.io.valid) 
+  val keyDownReg = RegEnable(isKeyDown, false.B, validPosEdge) 
 
   val scanDcdLed1 = Module(new DecoderLed)
   val scanDcdLed2 = Module(new DecoderLed)
@@ -140,7 +141,7 @@ class Exp7() extends Module {
   io.led7Ascii := asciiDcdLed2.io.out ## asciiDcdLed1.io.out
 
   val cntReg = RegInit(0.U(8.W))
-  cntReg := Mux(ps.io.valid, cntReg + (isKeyDown & ~keyDownReg), cntReg)
+  cntReg := Mux(validPosEdge, cntReg + (isKeyDown & ~keyDownReg), cntReg)
   val cntDcdLed1 = Module(new DecoderLed)
   val cntDcdLed2 = Module(new DecoderLed)
   cntDcdLed1.io.en := true.B
