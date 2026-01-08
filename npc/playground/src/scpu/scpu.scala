1b2c70cf7570b7e276f324f0aaa3ddcf0534cf3d
tracer-ysyx <tracer@ysyx.org>
2026-01-08 12:31:42 +0800

    > generate verilog ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:31:42 up 32 min,  1 user,  load average: 0.85, 0.28, 0.10

--- npc/playground/src/scpu/scpu.scala
+++ npc/playground/src/scpu/scpu.scala
@@ -248,7 +248,7 @@ class SCpu extends Module {
   val led7Dcd = Seq.fill(2)(Module(new DecoderLed))
   led7Dcd(0).io.in := gpr.io.rData(1)(3, 0)
   led7Dcd(1).io.in := gpr.io.rData(1)(7, 4)
-  val isWriteLed7 = decoder.io.selSignal.isWriteLed7 === 1.U
+  val isWriteLed7 = decoder.io.selSignal.isWriteLed7
   led7Reg(0) := Mux(isWriteLed7, led7Dcd(0).io.out, led7Reg(0))
   led7Reg(1) := Mux(isWriteLed7, led7Dcd(1).io.out, led7Reg(1))
   io.led7 := led7Reg.asUInt
