7cccaa698f4f6893904501a21338c5f43e5b95e4
tracer-ysyx <tracer@ysyx.org>
2026-01-10 09:45:42 +0800

    > generate verilog ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:45:42 up 28 min,  1 user,  load average: 1.76, 0.51, 0.17

--- npc/playground/src/DLE/exp2.scala
+++ npc/playground/src/DLE/exp2.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 
 class DecoderLed extends Module {
   val io = IO(new Bundle {
-    val in  = Input(UInt(4.W))
+    val in = Input(UInt(4.W))
     val out = Output(UInt(7.W))
   })
 
@@ -58,7 +58,7 @@ class PEncoder83 extends Module {
   })
 
   val hotIn = VecInit.fill(8)(false.B)
-  val cond  = VecInit.fill(8)(false.B)
+  val cond = VecInit.fill(8)(false.B)
 
   hotIn(7) := io.in(7)
   cond(7) := ~io.in(7)
@@ -75,22 +75,22 @@ class PEncoder83 extends Module {
 
 class Exp2 extends Module {
   val io = IO(new Bundle {
-    val sw     = Input(UInt(8.W))
-    val en     = Input(Bool())
-    val led    = Output(UInt(3.W))
+    val sw = Input(UInt(8.W))
+    val en = Input(Bool())
+    val led = Output(UInt(3.W))
     val hasOne = Output(Bool())
-    val led7   = Output(UInt(7.W))
+    val led7 = Output(UInt(7.W))
   })
 
-  val pe     = Module(new PEncoder83)
+  val pe = Module(new PEncoder83)
   val decLed = Module(new DecoderLed)
 
   io.hasOne := io.sw.orR
   
-  pe.io.in  := io.sw
-  pe.io.en  := io.en
-  io.led    := pe.io.out
+  pe.io.in := io.sw
+  pe.io.en := io.en
+  io.led := pe.io.out
 
   decLed.io.in := 0.U(1.W) ## io.led
-  io.led7      := decLed.io.out
+  io.led7 := decLed.io.out
 }
