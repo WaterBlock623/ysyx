e8663e6ca90f866a1c5300ded6e584b9f1089791
tracer-ysyx <tracer@ysyx.org>
2026-01-10 09:59:43 +0800

    > compile NEMU ysyx_22040000 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:59:43 up 42 min,  1 user,  load average: 0.09, 0.11, 0.15

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -165,6 +165,13 @@ static int cmd_d(char *args, char *str_len) {
   return 0;
 }
 
+static int cmd_p(char *args, char *str_len) {
+  bool success = true;
+  uint32_t val = expr(args, &success);
+  printf("%u\n", val);
+  return 0;
+}
+
 static struct {
   const char *name;
   const char *description;
@@ -178,6 +185,7 @@ static struct {
   { "x", "Print 4N byte from memory that beginning address is Expr", cmd_x },
   { "w", "New a watchpoint", cmd_w },
   { "d", "Delete a watchpoint through NO", cmd_d },
+  { "p", "", cmd_p }
 };
 
 #define NR_CMD ARRLEN(cmd_table)
