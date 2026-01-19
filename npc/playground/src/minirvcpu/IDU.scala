4bf4eb413c3aa102f9c76f1fd9f1a370a1fefb67
tracer-ysyx <tracer@ysyx.org>
2026-01-19 17:10:39 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:10:39 up  5:49,  1 user,  load average: 0.24, 0.22, 0.12

--- npc/playground/src/minirvcpu/IDU.scala
+++ npc/playground/src/minirvcpu/IDU.scala
@@ -3,9 +3,13 @@ package minirvcpu
 import chisel3._
 import chisel3.util.experimental.decode._
 import chisel3.util.BitPat
+import scala.collection.immutable.ListMap
+import scala.language.dynamics
 
 trait HasMoreSignalInfo {
+  def extType: ExtType.Value
   def stage: String
+  def chiselType: Data
 }
 
 case class InstPatternMaker(
@@ -30,6 +34,7 @@ object InstFields {
   val fieldsBase = Seq(
     new BoolDecodeField[InstPatternMaker] with HasMoreSignalInfo {
       def name = "isWriteBackReg"
+      def extType = ExtType.I
       def stage = "wb"
       def genTable(i: InstPatternMaker) = if (i.isWriteBackReg) y else n
     }
@@ -65,11 +70,31 @@ case class InstDecodeCollector()(implicit private val cfg: CoreConfig) {
     genSeq(fieldMap)
 }
 
+abstract class DynamicRecord(elementsMap: ListMap[String, Data]) extends Record with Dynamic {
+  val elements = elementsMap
+  def selectDynamic(name: String): Data = {
+    elements.getOrElse(name, throw new IllegalArgumentException(
+      s"Field $name not found in Record. Available: ${elements.keys.mkString(", ")}"))
+  }
+}
+
+class StageSignals(fields: Seq[DecodeField[InstPatternMaker, _] with HasMoreSignalInfo])
+  extends DynamicRecord(ListMap(fields.map(f => f.name -> f.chiselType.cloneType): _*))
+
+class AutoCtrlSignals(allFields: Seq[DecodeField[InstPatternMaker, _] with HasMoreSignalInfo])
+  extends DynamicRecord({
+    val grouped = allFields.groupBy(_.stage)
+    ListMap(grouped.toSeq.sortBy(_._1).map { case (stageName, stageFields) =>
+      stageName -> new StageSignals(stageFields)
+    }: _*)
+  })
+
 class IDU(implicit val cfg: CoreConfig) extends Module {
-  val ctrlSignals = IO(new CtrlSignals)
   val inst = IO(Input(UInt(cfg.xlen.W)))
 
   val decodeCollector = InstDecodeCollector()
+  val ctrlSignals = IO(new AutoCtrlSignals(decodeCollector.allFields))
+
   val decodeTable = new DecodeTable(decodeCollector.allPatterns, decodeCollector.allFields)
   val decodeResult = decodeTable.decode(inst)
   decodeCollector.allFields.foreach { f =>
