11c2191a3f8cac2e24060301bb20d2661f0925a4
tracer-ysyx <tracer@ysyx.org>
2026-01-19 21:41:31 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:41:30 up 10:28,  1 user,  load average: 0.19, 0.26, 0.38

--- npc/generator/src/minirvcpu/GenCtrlSignals.scala
+++ npc/generator/src/minirvcpu/GenCtrlSignals.scala
@@ -5,5 +5,5 @@ import cpuutil.BundleGenerator
 object GenCtrlSignals extends App {
   val collector = InstDecodeCollector()
   val gen = new BundleGenerator("minirvcpu", "CtrlSignals", collector.allFields) 
-  gen.generate(args(0) + "CtrlSignals.scala")
+  gen.generate(args(0) + "/CtrlSignals.scala")
 }
