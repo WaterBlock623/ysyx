acc904b5b04ad6ba38ef93438786afd0f4d33fe4
tracer-ysyx <tracer@ysyx.org>
2026-01-21 12:26:57 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:26:57 up  1:47,  1 user,  load average: 5.74, 3.02, 1.53

--- npc/common/src/minirvcpu/Instructions.scala
+++ npc/common/src/minirvcpu/Instructions.scala
@@ -37,6 +37,7 @@ case class InstPattern(inst: rvdecoderdb.Instruction) {
 
 object InstPatternss {
   print("xxxyyyzzz")
+  print(os.pwd / "rvdecoderdb" / "riscv-opcodes")
   val allInsts = rvdecoderdb.instructions(os.pwd / "rvdecoderdb" / "riscv-opcodes")
   print("abc :" + allInsts.toString)
   //val i = allInsts.filter(inst => inst.pseudoFrom.isEmpty &&
