1a836b0580e917948eff8b01b4ccb397a23ba2d7
tracer-ysyx <tracer@ysyx.org>
2026-01-22 22:20:46 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:20:46 up 11:36,  1 user,  load average: 0.22, 0.41, 0.30

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -62,7 +62,9 @@ uint32_t M[1 << 22] = {
 extern "C" uint32_t pmem_read(uint32_t raddr) {
 	printf("rAddr: %#.8x\n", raddr);
 	raddr -= 0x80000000;
-	return M[raddr >> 2];
+	if (raddr != 0)
+		return M[raddr >> 2];
+	else return 0;
 }
 extern "C" void pmem_write(uint32_t waddr, uint32_t wdata, unsigned char wmask) {
 	printf("wAddr: %#.8x  data: %#.8x  mask: %#.8x\n", waddr, wdata, wmask);
