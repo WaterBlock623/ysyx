5ea5671e9745d4f72ace70daea8984f141aa1555
tracer-ysyx <tracer@ysyx.org>
2026-01-22 13:14:22 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:14:22 up  2:39,  1 user,  load average: 2.07, 1.38, 1.02

--- npc/playground/src/minirvcpu/Elaborate.scala
+++ npc/playground/src/minirvcpu/Elaborate.scala
@@ -14,6 +14,6 @@ object Elaborate extends App {
   val rootStr = System.getProperty("project.root")
   val workspacePath = os.Path(rootStr)
   val cfg = CoreConfig(rvOpcodesPath = workspacePath / "rvdecoderdb" / "riscv-opcodes")
-  println(firtoolOptions)
+  firtoolOptions.foreach(s => println(s))
   circt.stage.ChiselStage.emitSystemVerilogFile(new minirvcpu.Top()(cfg), args, firtoolOptions)
 }
