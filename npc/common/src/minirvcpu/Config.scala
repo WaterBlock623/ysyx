4641f7682a6cfc3ce3df51b8027c40e00ab72e96
tracer-ysyx <tracer@ysyx.org>
2026-01-22 22:07:48 +0800

    > sim RTL ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:07:48 up 11:23,  1 user,  load average: 1.31, 0.42, 0.18

--- npc/common/src/minirvcpu/Config.scala
+++ npc/common/src/minirvcpu/Config.scala
@@ -6,7 +6,7 @@ case class CoreConfig(
   val extensions:          Set[ExtTypeEnum.Type] = Set(ExtTypeEnum.I),
   val registerAddrWidth:   Int = 4,
   val registerReadPortNum: Int = 2,
-  val memoryAddrWidth:     Int = 24) {
+  val memoryAddrWidth:     Int = 32) {
   require(xlen == 32 || xlen == 64)
   val registerNum: Int = 1 << registerAddrWidth
   require(memoryAddrWidth <= 32)
