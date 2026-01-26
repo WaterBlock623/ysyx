cfe504590d01d0314da82e4c895c44322557edc7
tracer-ysyx <tracer@ysyx.org>
2026-01-26 12:25:57 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:25:57 up  2:13,  1 user,  load average: 0.04, 0.04, 0.00

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
+#include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
@@ -34,9 +35,17 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  // panic("Not implemented");
 #endif
-  return NULL;
+  static char *addr = NULL;
+  if (addr == NULL) {
+    addr = (void *)ROUNDUP(heap.start, 8);
+  }
+  size = ROUNDUP(size, 8);
+  char *old = addr;
+  addr += size;
+  assert((uintptr_t)addr < (uintptr_t)heap.end);
+  return old; 
 }
 
 void free(void *ptr) {
