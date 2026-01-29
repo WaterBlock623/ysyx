fac5d9d3a561ac66adc0d91f5ce4412cb0f59a77
tracer-ysyx <tracer@ysyx.org>
2026-01-29 19:08:32 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:08:32 up  8:04,  1 user,  load average: 0.01, 0.02, 0.00

--- nemu/src/utils/elfparser.c
+++ nemu/src/utils/elfparser.c
@@ -18,7 +18,7 @@ static elf_ehdr_t eh;
 static elf_shdr_t sh[SH_MAX];
 static char sh_name[SH_MAX][SH_NAME_MAX];
 static elf_sym_t sym[SYM_MAX];
-//static char sym_name[SYM_MAX][SYM_NAME_MAX];
+static char sym_name[SYM_MAX][SYM_NAME_MAX];
 
 static size_t fread_assert(void *ptr, size_t byte, FILE *stream) {
   size_t ret = fread(ptr, 1, byte, stream);
@@ -38,21 +38,13 @@ static char *fstrncpy(char *dest, FILE *stream, unsigned long n) {
   return dest;
 }
 
-void init_elf(const char *elf_file) {
-  if (elf_file == NULL) {
-    Log("No ELF file");
-    return;
-  }
-  FILE *elf = fopen(elf_file, "r");
-  Assert(elf, "Cant open ELF file");
-  Log("ELF file: %s", elf_file);
-
-  // ELF header
+static void parse_elf_header(FILE *elf) {
   fread_assert(&eh, sizeof(eh), elf);
   Log("ELF header: off: %u  size: %u  num: %u  strndx: %u\n", 
       eh.e_shoff, eh.e_shentsize, eh.e_shnum, eh.e_shstrndx);
+}
 
-  // Section header
+static void parse_section_header(FILE *elf) {
   Assert(eh.e_shnum <= SH_MAX, "SH_MAX is not enough. At least %u", eh.e_shnum);
   int i;
   fseek(elf, eh.e_shoff, SEEK_SET);
@@ -62,24 +54,25 @@ void init_elf(const char *elf_file) {
   }
 
   // Section header name
-  word_t sh_shstrtab;
+  word_t shstrtab;
   if (eh.e_shstrndx == SHN_XINDEX) {
     Assert(sh[0].sh_link != 0, "Invalid shstrtab");
-    sh_shstrtab = sh[0].sh_link;
+    shstrtab = sh[0].sh_link;
   } else {
-    sh_shstrtab = sh[eh.e_shstrndx].sh_offset;
+    shstrtab = sh[eh.e_shstrndx].sh_offset;
   }
   for (i = 0; i < eh.e_shnum; i++) {
-    fseek(elf, sh_shstrtab + sh[i].sh_name, SEEK_SET);
+    fseek(elf, shstrtab + sh[i].sh_name, SEEK_SET);
     fstrncpy(sh_name[i], elf, SH_NAME_MAX);
-    sh_name[i][SH_NAME_MAX - 1] = '\0';
     Log("Section header %d: "
         "name: %s  addr: %#x  off: %#x  size: %u  entsize: %u\n", 
         i, sh_name[i], sh[i].sh_addr, sh[i].sh_offset, 
         sh[i].sh_size, sh[i].sh_entsize);
   }
+}
 
-  // Symbol table
+static void parse_symbol_table(FILE *elf) {
+  int i;
   word_t sh_symtab;
   for (i = 0; i < eh.e_shnum; i++) {
     if (sh[i].sh_type == SHT_SYMTAB) {
@@ -95,4 +88,33 @@ void init_elf(const char *elf_file) {
     elf_sym_t *s = sym + i;
     fread_assert(s, sh[sh_symtab].sh_entsize, elf);
   } 
+
+  // Symbol table name
+  word_t sh_strtab = sh[sh_symtab].sh_link;
+  word_t strtab = sh[sh_strtab].sh_offset;
+  for (i = 0; i < symtab_num; i++) {
+    fseek(elf, strtab + sym[i].st_name, SEEK_SET);
+    fstrncpy(sym_name[i], elf, SYM_NAME_MAX);
+    Log("Symbol %d: name: %s  value: %#x  size: %u", 
+        i, sym_name[i], sym[i].st_value, sym[i].st_size);
+  } 
+}
+
+void init_elf(const char *elf_file) {
+  if (elf_file == NULL) {
+    Log("No ELF file");
+    return;
+  }
+  FILE *elf = fopen(elf_file, "r");
+  Assert(elf, "Cant open ELF file");
+  Log("ELF file: %s", elf_file);
+
+  // ELF header
+  parse_elf_header(elf);
+
+  // Section header
+  parse_section_header(elf);
+
+  // Symbol table
+  parse_symbol_table(elf);
 }
