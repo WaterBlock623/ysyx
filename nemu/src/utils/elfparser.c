8e375e587aba23a27c891c4f4d78e1e9997d770b
tracer-ysyx <tracer@ysyx.org>
2026-01-29 18:49:18 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:49:18 up  7:46,  1 user,  load average: 0.09, 0.04, 0.01

--- nemu/src/utils/elfparser.c
+++ nemu/src/utils/elfparser.c
@@ -1,5 +1,4 @@
 #include "debug.h"
-#include <SDL2/SDL_stdinc.h>
 #include <common.h>
 #include <elf.h>
 #include <stdio.h>
@@ -12,15 +11,31 @@ typedef MUXDEF(CONFIG_ISA64, Elf64_Sym, Elf32_Sym) elf_sym_t;
 
 #define SH_MAX 64
 #define SH_NAME_MAX 128
+#define SYM_MAX 512
+#define SYM_NAME_MAX 128
 
 static elf_ehdr_t eh;
 static elf_shdr_t sh[SH_MAX];
 static char sh_name[SH_MAX][SH_NAME_MAX];
+static elf_sym_t sym[SYM_MAX];
+//static char sym_name[SYM_MAX][SYM_NAME_MAX];
 
 static size_t fread_assert(void *ptr, size_t byte, FILE *stream) {
   size_t ret = fread(ptr, 1, byte, stream);
   Assert(ret == byte, "fread fail: expect %lu bytes but %lu byte", byte, ret);
   return byte;
+}
+
+static char *fstrncpy(char *dest, FILE *stream, unsigned long n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    fread_assert(dest, 1, stream);
+    if (*dest++ == '\0') {
+      break;
+    }
+  }
+  *dest = '\0';
+  return dest;
 }
 
 void init_elf(const char *elf_file) {
@@ -47,16 +62,16 @@ void init_elf(const char *elf_file) {
   }
 
   // Section header name
-  word_t shstrtab;
+  word_t sh_shstrtab;
   if (eh.e_shstrndx == SHN_XINDEX) {
     Assert(sh[0].sh_link != 0, "Invalid shstrtab");
-    shstrtab = sh[0].sh_link;
+    sh_shstrtab = sh[0].sh_link;
   } else {
-    shstrtab = sh[eh.e_shstrndx].sh_offset;
+    sh_shstrtab = sh[eh.e_shstrndx].sh_offset;
   }
   for (i = 0; i < eh.e_shnum; i++) {
-    fseek(elf, shstrtab + sh[i].sh_name, SEEK_SET);
-    fread_assert(sh_name[i], SH_NAME_MAX, elf);
+    fseek(elf, sh_shstrtab + sh[i].sh_name, SEEK_SET);
+    fstrncpy(sh_name[i], elf, SH_NAME_MAX);
     sh_name[i][SH_NAME_MAX - 1] = '\0';
     Log("Section header %d: "
         "name: %s  addr: %#x  off: %#x  size: %u  entsize: %u\n", 
@@ -64,5 +79,20 @@ void init_elf(const char *elf_file) {
         sh[i].sh_size, sh[i].sh_entsize);
   }
 
-
+  // Symbol table
+  word_t sh_symtab;
+  for (i = 0; i < eh.e_shnum; i++) {
+    if (sh[i].sh_type == SHT_SYMTAB) {
+      sh_symtab = i;
+      break;
+    }
+  }
+  Assert(i < eh.e_shnum, "symtab is not found");
+  word_t symtab_num = sh[sh_symtab].sh_size / sh[sh_symtab].sh_entsize;
+  Assert(symtab_num <= SYM_MAX, "SYM_MAX is not enough. At least %u", symtab_num);
+  fseek(elf, sh[sh_symtab].sh_offset, SEEK_SET);
+  for (i = 0; i < symtab_num; i++) {
+    elf_sym_t *s = sym + i;
+    fread_assert(s, sh[sh_symtab].sh_entsize, elf);
+  } 
 }
