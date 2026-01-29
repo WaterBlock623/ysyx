a86ab588b72c8c837e10514e3f3d9ac76ed71930
tracer-ysyx <tracer@ysyx.org>
2026-01-29 22:04:44 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:04:44 up 10:58,  1 user,  load average: 0.06, 0.03, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -72,7 +72,7 @@ static void ftrace(int rd, int rs1, paddr_t pc, paddr_t dnpc) {
 #define FUNC_NAME_MAX 128
 #define FRONT_MSG "FTrace: "FMT_PADDR": ", pc
 #define RET_MSG "ret  [%s]\n", pc_func_name
-#define CALL_MSG "call [%s@"FMT_PADDR"]", dnpc_func_name, dnpc
+#define CALL_MSG "call [%s@"FMT_PADDR"]\n", dnpc_func_name, dnpc
 #define PRINT_MSG(cmd) do { \
   if (is_ret) { \
     cmd(FRONT_MSG); \
