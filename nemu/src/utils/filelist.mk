6ebbd504fc49b1b1d2f99283c35ee8785901136f
tracer-ysyx <tracer@ysyx.org>
2026-02-01 15:30:13 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:30:13 up  1:26,  1 user,  load average: 0.52, 0.14, 0.11

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -14,10 +14,10 @@
 #**************************************************************************************/
 
 ifeq ($(CONFIG_ITRACE)$(CONFIG_IQUEUE),)
-SRCS-BLACKLIST-y += src/utils/disasm.c
+SRCS-BLACKLIST-y += $(NEMU_HOME)/src/utils/disasm.c
 else
-LIBCAPSTONE = tools/capstone/repo/libcapstone.so.5
-CFLAGS += -I tools/capstone/repo/include
+LIBCAPSTONE = $(NEMU_HOME)/tools/capstone/repo/libcapstone.so.5
+CFLAGS += -I $(NEMU_HOME)/tools/capstone/repo/include
 src/utils/disasm.c: $(LIBCAPSTONE)
 $(LIBCAPSTONE):
 	$(MAKE) -C tools/capstone
