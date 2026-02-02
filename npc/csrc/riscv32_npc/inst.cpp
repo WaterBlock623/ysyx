f8842df3d2ab77aa51a05d1bdc236a097cc60ee8
tracer-ysyx <tracer@ysyx.org>
2026-02-02 21:37:30 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:37:30 up 10:59,  1 user,  load average: 0.36, 0.19, 0.12

--- npc/csrc/riscv32_npc/inst.cpp
+++ npc/csrc/riscv32_npc/inst.cpp
@@ -41,7 +41,7 @@ int isa_exec_once(Decode *s) {
   IFDEF(CONFIG_ITRACE, print_disassemble(s));
   single_cycle(); 
   sync_npc_gpr();
-  if (s->pc >= 0x80000010) {
+  if (s->pc >= 0x8000000c) {
     sim_close();
     panic("SIM CLOSE");
   }
