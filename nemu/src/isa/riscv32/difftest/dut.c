686740f2b418a138e42cf5ea806175b58d830068
tracer-ysyx <tracer@ysyx.org>
2026-02-03 23:56:55 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:56:55 up 12:14,  1 user,  load average: 0.08, 0.02, 0.05

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -1,34 +1,35 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ * PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
-#include <isa.h>
-#include <cpu/difftest.h>
 #include "../local-include/reg.h"
 #include "utils.h"
+#include <cpu/difftest.h>
+#include <isa.h>
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int i;
   for (i = 0; i < LENGTH(cpu.gpr); i++) {
+    printf("reg %d : nemu: " FMT_WORD "  ref: " FMT_WORD "\n", i, gpr(i),
+           ref_r->gpr[i]);
     if (gpr(i) != ref_r->gpr[i]) {
-      printf("reg %d fail: nemu: " FMT_WORD "  ref: " FMT_WORD "\n", 
-          i, gpr(i), ref_r->gpr[i]);
+      // printf("reg %d fail: nemu: " FMT_WORD "  ref: " FMT_WORD "\n", i, gpr(i),
+      //        ref_r->gpr[i]);
       return false;
     }
   }
   return true;
 }
 
-void isa_difftest_attach() {
-}
+void isa_difftest_attach() {}
