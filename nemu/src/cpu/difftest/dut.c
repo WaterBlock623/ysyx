945032c47202464df2113bf95cae79e1848913fd
tracer-ysyx <tracer@ysyx.org>
2026-02-04 00:26:45 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:26:45 up 12:44,  1 user,  load average: 0.11, 0.23, 0.22

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -110,8 +110,8 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 void difftest_step(vaddr_t pc, vaddr_t npc) {
   // CPU_state ref_r;
   struct {
-    word_t gpr[32];
     vaddr_t pc;
+    word_t gpr[32];
   } ref_r;
 
   if (skip_dut_nr_inst > 0) {
