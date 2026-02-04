17308d663e730cdb540e0ad5f511f9dd2bf0f5a2
tracer-ysyx <tracer@ysyx.org>
2026-02-04 19:50:02 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:50:02 up  9:42,  1 user,  load average: 0.10, 0.17, 0.10

--- npc/scripts/verilator.mk
+++ npc/scripts/verilator.mk
@@ -42,7 +42,7 @@ INC_PATH := $(WORK_DIR)/csrc/$(GUEST_ISA)/include \
 						$(WORK_DIR)/include $(NEMU_HOME)/include $(INC_PATH)
 export ADD_INC_PATH := $(INC_PATH)
 INCFLAGS = $(addprefix -I, $(INC_PATH))
-CXXFLAGS += $(INCFLAGS) -D__TOP_NAME__="V$(TOPNAME)" -D__TOP_NAME_INCLUDE__='"V$(TOPNAME).h"'
+CXXFLAGS += $(INCFLAGS) -D__TOP_NAME__="V$(TOPNAME)" -D__TOP_NAME_INCLUDE__=''"V$(TOPNAME).h"''
 CXXFLAGS += -D__WAVE__=$(WAVE)
 
 NEMU_MAKE_FLAGS += WORK_DIR="$(WORK_DIR)" \
