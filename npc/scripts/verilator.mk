20489a8a3833c4c8d1db53827a3a1b33528938b4
tracer-ysyx <tracer@ysyx.org>
2026-02-04 18:30:05 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:30:05 up  8:19,  1 user,  load average: 0.09, 0.10, 0.08

--- npc/scripts/verilator.mk
+++ npc/scripts/verilator.mk
@@ -7,7 +7,7 @@ WAVE = $(WAVE_DIR)/sim.fst
 $(shell mkdir -p $(OBJ_DIR))
 $(shell mkdir -p $(WAVE_DIR))
 
-VERILATOR_CFLAGS += -MMD --cc \
+VERILATOR_CFLAGS += -MMD --cc --build \
 				-O3 --x-assign fast --x-initial fast --noassert
 
 ifeq ($(CONFIG_NPC_WAVE),y)
