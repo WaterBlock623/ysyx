3acabf75097ec7ad4642c4918ba9eee6292e6632
tracer-ysyx <tracer@ysyx.org>
2026-02-05 15:43:16 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:43:16 up  5:04,  1 user,  load average: 0.07, 0.03, 0.00

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -11,6 +11,9 @@ WORK_DIR  ?= $(shell pwd)
 BUILD_DIR ?= $(WORK_DIR)/build
 
 INC_PATH := $(ADD_INC_PATH) $(WORK_DIR)/include $(NEMU_HOME)/include $(INC_PATH)
+# ifneq ($(CONFIG_NPC),)
+# INC_PATH := $(ADD_INC_PATH) $(INC_PATH)
+# endif
 $(info NEMU INC_PATH $(INC_PATH))
 OBJ_DIR  = $(BUILD_DIR)/obj-$(NAME)$(SO)
 BINARY   = $(BUILD_DIR)/$(NAME)$(SO)
