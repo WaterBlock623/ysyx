fa60ae338c4e7ac0fbcdcd25febaeba52f6bed99
tracer-ysyx <tracer@ysyx.org>
2026-02-07 16:46:08 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:46:08 up  2:34,  1 user,  load average: 0.11, 0.55, 0.62

--- npc/generator/src/sirius/GenCtrlSignals.scala
+++ npc/generator/src/sirius/GenCtrlSignals.scala
@@ -7,8 +7,7 @@ object GenCtrlSignals extends App {
   val workspacePath = os.Path(rootStr)
   val rvOpCodesPath = workspacePath / "rvdecoderdb" / "riscv-opcodes"
   println(rvOpCodesPath)
-  val cfg =
-    CoreConfig(rvOpCodesPath = rvOpCodesPath)
+  val cfg = CoreConfig(xlen = 1, rvOpCodesPath = rvOpCodesPath)
   val collector = InstDecodeCollector()(cfg)
   val gen = new BundleGenerator("sirius", "CtrlSignals", collector.allFields)
   gen.generate(args(0) + "/CtrlSignals.scala")
