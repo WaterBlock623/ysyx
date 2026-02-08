ad4c77c1737f49eed3e927dca0ead396721dd1d7
tracer-ysyx <tracer@ysyx.org>
2026-02-08 17:33:01 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:33:01 up  8:07,  1 user,  load average: 2.70, 1.59, 0.96

--- npc/common/src/sirius/rvi/InstPatterns.scala
+++ npc/common/src/sirius/rvi/InstPatterns.scala
@@ -275,6 +275,17 @@ case class InstPatternRvI(
       jumpTargetSel = JumpTargetSelEnum.pcPlusImm
     ),
     InstPattern(
+      "bge",
+      ExtTypeEnum.I,
+      InstTypeEnum.B,
+      aluIn1Sel = AluInSelEnum.rs,
+      aluIn2Sel = AluInSelEnum.rs,
+      aluOp = AluOpEnum.ge,
+      exuOutSel = ExuOutSelEnum.aluBase,
+      isBranch = true,
+      jumpTargetSel = JumpTargetSelEnum.pcPlusImm
+    ),
+    InstPattern(
       "ebreak",
       ExtTypeEnum.I,
       DontCare
