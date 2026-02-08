38ee7c06e4cd7cbc5f4a71db217ecd5cd686d358
tracer-ysyx <tracer@ysyx.org>
2026-02-08 18:47:45 +0800

    > sim RTL ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:47:45 up  9:22,  1 user,  load average: 0.54, 0.61, 0.38

--- nemu/include/cpu/difftest.h
+++ nemu/include/cpu/difftest.h
@@ -29,6 +29,7 @@ void difftest_detach();
 void difftest_attach();
 #else
 static inline void difftest_skip_ref() {}
+static inline void difftest_skip_next_ref() {}
 static inline void difftest_skip_dut(int nr_ref, int nr_dut) {}
 static inline void difftest_set_patch(void (*fn)(void *arg), void *arg) {}
 static inline void difftest_step(vaddr_t pc, vaddr_t npc) {}
