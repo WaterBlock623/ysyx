45223c135828277a1c647736e038e8d2488d91cf
tracer-ysyx <tracer@ysyx.org>
2026-02-08 14:55:39 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:55:39 up  5:29,  1 user,  load average: 1.31, 0.81, 0.64

--- npc/playground/src/sirius/Exu.scala
+++ npc/playground/src/sirius/Exu.scala
@@ -33,19 +33,19 @@ class AluBase(
     extends AluParent {
   import AluOpEnum._
 
-  val isSub = io.aluOp === sub.asUInt
-  val negSrc2 = ~io.src2 + 1.U
-  val addSubResult = io.src1 + Mux(isSub, negSrc2, io.src2)
-
-  io.out := MuxLookup(io.aluOp, addSubResult)(
-    Seq(
-      add.asUInt -> addSubResult,
-      sub.asUInt -> addSubResult,
-    )
-  )
+  // val isSub = io.aluOp === sub.asUInt
+  // val negSrc2 = ~io.src2 + 1.U
+  // val addSubResult = io.src1 + Mux(isSub, negSrc2, io.src2)
+  //
+  // io.out := MuxLookup(io.aluOp, addSubResult)(
+  //   Seq(
+  //     add.asUInt -> addSubResult,
+  //     sub.asUInt -> addSubResult,
+  //   )
+  // )
   
-  // val addResult = io.src1 + io.src2
-  // val subResult = io.src1 - io.src2
+  val addResult = io.src1 + io.src2
+  val subResult = io.src1 - io.src2
   // // // val ltResult = io.src1 < io.src2
   // // // val andResult = io.src1 & io.src2
   // // // val orResult = io.src1 | io.src2
@@ -54,19 +54,19 @@ class AluBase(
   // // // val srlResult = io.src1 >> io.src2
   // // // val sraResult = (io.src1.asSInt >> io.src2).asUInt
   // //
-  // io.out := MuxLookup(io.aluOp, addResult)(
-  //   Seq(
-  //     add.asUInt -> addResult,
-  //     sub.asUInt -> subResult,
-  //     // lt.asUInt -> ltResult,
-  //     // and.asUInt -> andResult,
-  //     // or.asUInt -> orResult,
-  //     // xor.asUInt -> xorResult,
-  //     // sll.asUInt -> sllResult,
-  //     // srl.asUInt -> srlResult,
-  //     // sra.asUInt -> sraResult,
-  //   )
-  // )
+  io.out := MuxLookup(io.aluOp, addResult)(
+    Seq(
+      add.asUInt -> addResult,
+      sub.asUInt -> subResult,
+      // lt.asUInt -> ltResult,
+      // and.asUInt -> andResult,
+      // or.asUInt -> orResult,
+      // xor.asUInt -> xorResult,
+      // sll.asUInt -> sllResult,
+      // srl.asUInt -> srlResult,
+      // sra.asUInt -> sraResult,
+    )
+  )
 }
 
 class Exu(implicit private val cfg: CoreConfig, 
