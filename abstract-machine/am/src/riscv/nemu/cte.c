ae97ff22102b0f90299b94dfff8a20fbc93d1dfe
tracer-ysyx <tracer@ysyx.org>
2026-02-10 13:55:38 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:55:38 up  5:20,  1 user,  load average: 0.00, 0.02, 0.00

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,6 +5,13 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+  int i;
+  for (i = 0; i < NR_REGS; i++) {
+    printf("x%d: 0x%8x\n", i, c->gpr[i]);
+  }
+  printf("mstatus%d: 0x%8x\n", i, c->mstatus);
+  printf("mcause%d: 0x%8x\n", i, c->mcause);
+  printf("mepc%d: 0x%8x\n", i, c->mepc);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -33,7 +33,7 @@ static void restart() {
   /* The zero register is always 0. */
   cpu.gpr[0] = 0;
 
-  // cpu.csr[CSR_MSTATUS] = 0x1800;
+  cpu.csr[CSR_MSTATUS] = 0x1800;
 }
 
 void init_isa() {
