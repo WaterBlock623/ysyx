9a476e134b03d7914c2d3df93f1d3b287e293b6e
tracer-ysyx <tracer@ysyx.org>
2026-02-11 14:06:28 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:06:28 up  4:16,  1 user,  load average: 0.00, 0.03, 0.00

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -76,6 +76,6 @@ word_t mmio_read(paddr_t addr, int len) {
 
 void mmio_write(paddr_t addr, int len, word_t data) {
   IOMap *map = fetch_mmio_map(addr);
-  IFDEF(CONFIG_DTRACE, dtrace(map, true, addr, len, data));
   map_write(addr, len, data, map);
+  IFDEF(CONFIG_DTRACE, dtrace(map, true, addr, len, data));
 }
