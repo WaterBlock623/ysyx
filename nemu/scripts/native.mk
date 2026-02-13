9ec479591b6c667d5dab73d75778d51e4ff23405
tracer-ysyx <tracer@ysyx.org>
2026-02-13 22:11:34 +0800

    > sim RTL ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:11:34 up 11:52,  1 user,  load average: 0.00, 0.03, 0.12

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -39,6 +39,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 run: run-env
 	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
+	-mv -f $(NEMU_HOME)/profile.vlt $(BUILD_DIR)/../profile.vlt
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
