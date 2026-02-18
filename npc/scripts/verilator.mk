db4b59a58777cee071ed61ea8f9d7fc197989f86
tracer-ysyx <tracer@ysyx.org>
2026-02-18 16:45:12 +0800

    > sim RTL ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:45:12 up  3:50,  1 user,  load average: 0.32, 0.11, 0.09

--- npc/scripts/verilator.mk
+++ npc/scripts/verilator.mk
@@ -20,7 +20,7 @@ endif
 # 				--prof-pgo $(VLT_ARGS)
 VERILATOR_CFLAGS += -MMD --cc --build -j 16 \
 				-O3 --x-assign fast --x-initial fast --noassert --threads 4 \
-				--prof-pgo --prof-exec –threads-dpi all $(VLT_ARGS)
+				--threads-dpi all --prof-pgo --prof-exec $(VLT_ARGS)
 ifeq ($(CONFIG_NPC_WAVE),y)
 $(info WAVE is enable)
 VERILATOR_CFLAGS += --trace-fst
