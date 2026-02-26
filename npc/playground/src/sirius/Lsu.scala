5c2d3f4da8dec7d61ef08a5266aa9f448301b1a7
tracer-ysyx <tracer@ysyx.org>
2026-02-26 16:27:39 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:27:39 up  5:27,  1 user,  load average: 2.74, 1.18, 0.65

--- npc/playground/src/sirius/Lsu.scala
+++ npc/playground/src/sirius/Lsu.scala
@@ -6,7 +6,9 @@ import chisel3.util.MuxLookup
 import chisel3.experimental.dataview._
 import chisel3.util.Decoupled
 
-class Lsu(implicit private val cfg: CoreConfig) extends Module {
+class Lsu(
+  implicit private val cfg: CoreConfig)
+    extends Module {
   if (cfg.xlen != 32) {
     throw new IllegalArgumentException("Unsupported xlen")
   }
@@ -31,28 +33,22 @@ class Lsu(implicit private val cfg: CoreConfig) extends Module {
   val state = RegInit(sIdle)
   val isMemAcc = ctrl.isLoad || ctrl.isStore
 
-  state := MuxLookup(state, sIdle)(Seq(
-    sIdle -> Mux(in.valid && isMemAcc, sBusy, sIdle),
-    sBusy -> Mux(exte.mem.respValid, sIdle, sBusy),
-    ))
+  state := MuxLookup(state, sIdle)(
+    Seq(
+      sIdle -> Mux(in.valid && isMemAcc, sBusy, sIdle),
+      sBusy -> Mux(exte.mem.respValid, sIdle, sBusy)
+    )
+  )
 
-  val isBypass    = state === sIdle && !isMemAcc
+  val isBypass = state === sIdle && !isMemAcc
   val isCompleted = state === sBusy && exte.mem.respValid
 
   in.ready := isBypass || isCompleted
-  out.valid := in.valid && isBypass || isCompleted
+  out.valid := (in.valid && isBypass) || isCompleted
 
-  val canSendReq = (state === sIdle) && in.valid && isMemAcc
+  val canSendReq = state === sIdle && in.valid && isMemAcc
 
   exte.mem.reqValid := !reset.asBool && canSendReq
-  exte.mem.addr     := addr
-  exte.mem.wEn      := ctrl.isStore
-
-  // val isRespValid = state === sBusy && exte.mem.respValid
-  // in.ready := (state === sIdle && !isMemAcc) || isRespValid
-  // out.valid := (state === sIdle && !isMemAcc && in.valid) || isRespValid
-
-  exte.mem.reqValid := !reset.asBool && state === sIdle && isMemAcc && in.valid
   exte.mem.addr := addr
   exte.mem.wEn := ctrl.isStore
 
@@ -61,39 +57,46 @@ class Lsu(implicit private val cfg: CoreConfig) extends Module {
   val rData = exte.mem.rData
   val byteData = rData.asTypeOf(Vec(cfg.xlen >> 3, UInt(8.W)))
   val lbu = byteData(rem)
-  val lbData = Mux(ctrl.isUnsignedLoad, 0.U((cfg.xlen - 8).W), 
-    Fill(cfg.xlen - 8, lbu(7))) ## lbu
-  val lhu = Mux(rem(1), rData(31, 16), rData(15, 0)) 
-  val lhData = Mux(ctrl.isUnsignedLoad, 0.U((cfg.xlen - 16).W), 
-    Fill(cfg.xlen - 16, lhu(15))) ## lhu
+  val lbData = Mux(
+    ctrl.isUnsignedLoad,
+    0.U((cfg.xlen - 8).W),
+    Fill(cfg.xlen - 8, lbu(7))
+  ) ## lbu
+  val lhu = Mux(rem(1), rData(31, 16), rData(15, 0))
+  val lhData = Mux(
+    ctrl.isUnsignedLoad,
+    0.U((cfg.xlen - 16).W),
+    Fill(cfg.xlen - 16, lhu(15))
+  ) ## lhu
   val lwData = rData(31, 0)
-  outBits.lsuPayload.lsu.loadData := MuxLookup(ctrl.loadStoreLength, lwData)(Seq(
-    LoadStoreLengthEnum.w.asUInt -> lwData,
-    LoadStoreLengthEnum.h.asUInt -> lhData,
-    LoadStoreLengthEnum.b.asUInt -> lbData,
-    ))
+  outBits.lsuPayload.lsu.loadData := MuxLookup(ctrl.loadStoreLength, lwData)(
+    Seq(
+      LoadStoreLengthEnum.w.asUInt -> lwData,
+      LoadStoreLengthEnum.h.asUInt -> lhData,
+      LoadStoreLengthEnum.b.asUInt -> lbData
+    )
+  )
 
   // store
   val regData = inBits.exuPayload.idu.rs2Data
   val sb = (regData(7, 0) << (rem * 8.U)).pad(cfg.xlen)
   val sh = (regData(15, 0) << (rem(1) * 16.U)).pad(cfg.xlen)
   val sw = regData(31, 0).pad(cfg.xlen)
-  exte.mem.wData := MuxLookup(ctrl.loadStoreLength, sw)(Seq(
-    LoadStoreLengthEnum.w.asUInt -> sw,
-    LoadStoreLengthEnum.h.asUInt -> sh,
-    LoadStoreLengthEnum.b.asUInt -> sb,
-    ))
+  exte.mem.wData := MuxLookup(ctrl.loadStoreLength, sw)(
+    Seq(
+      LoadStoreLengthEnum.w.asUInt -> sw,
+      LoadStoreLengthEnum.h.asUInt -> sh,
+      LoadStoreLengthEnum.b.asUInt -> sb
+    )
+  )
   val sbMask = (1.U << rem).pad(cfg.xlen)
   val shMask = (3.U << (rem & 2.U)).pad(cfg.xlen)
   val swMask = 15.U(cfg.xlen.W)
-  exte.mem.wMask := MuxLookup(ctrl.loadStoreLength, swMask)(Seq(
-    LoadStoreLengthEnum.w.asUInt -> swMask,
-    LoadStoreLengthEnum.h.asUInt -> shMask,
-    LoadStoreLengthEnum.b.asUInt -> sbMask,
-    ))
+  exte.mem.wMask := MuxLookup(ctrl.loadStoreLength, swMask)(
+    Seq(
+      LoadStoreLengthEnum.w.asUInt -> swMask,
+      LoadStoreLengthEnum.h.asUInt -> shMask,
+      LoadStoreLengthEnum.b.asUInt -> sbMask
+    )
+  )
 }
-
-
-
-
-
