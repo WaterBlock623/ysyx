24b0a62ccaee37398b4ff9ada2674a7e764c7b6d
tracer-ysyx <tracer@ysyx.org>
2026-02-26 23:58:50 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:58:50 up 12:58,  1 user,  load average: 0.93, 0.76, 0.49

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -34,7 +34,7 @@ class Ifu(
   // exte.mem.reqValid := !reset.asBool && (state === sIdle || (state === sWait && out.ready))
   // val isSBusy = state === sBusy
   // exte.mem.reqValid := isSBusy && !RegNext(isSBusy)
-  exte.mem.reqValid := (state === sBusy && !exte.mem.respValid) || (state === sWait && out.ready)
+  exte.mem.reqValid := !reset.asBool && ((state === sBusy && !exte.mem.respValid) || (state === sWait && out.ready))
 
   //
   // import chisel3.util.random.LFSR
