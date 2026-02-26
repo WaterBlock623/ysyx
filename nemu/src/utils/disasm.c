6e7b0addfa07866b6192a6dc8e03e7d84bc9cb1b
tracer-ysyx <tracer@ysyx.org>
2026-02-26 21:35:40 +0800

    > sim RTL ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:35:39 up 10:35,  1 user,  load average: 0.01, 0.13, 0.45

--- nemu/src/utils/disasm.c
+++ nemu/src/utils/disasm.c
@@ -71,7 +71,7 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
 	cs_insn *insn;
 	size_t count = cs_disasm_dl(handle, code, nbyte, pc, 0, &insn);
   // Assert(count == 1, "Unknown instruction");
-  if (count == 1) {
+  if (count != 1) {
     printf(ANSI_FG_RED "Unknown instruction at 0x%016lx: 0x", pc);
     int i;
     for (i = nbyte - 1; i >= 0; i--) {
