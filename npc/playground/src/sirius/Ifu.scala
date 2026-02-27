9aaac4ff238e70071992bb5e50064c48968533cf
tracer-ysyx <tracer@ysyx.org>
2026-02-27 13:38:54 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:38:54 up  3:36,  1 user,  load average: 0.83, 0.72, 0.56

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -36,7 +36,7 @@ class Ifu(
   
   out.bits := outBits
 
-  exte.mem.reqValid := !RegNext(reset.asBool) && state === sBusy
+  exte.mem.reqValid := state === sBusy
 
   // out.bits := Mux(
   //   exte.mem.respValid,
