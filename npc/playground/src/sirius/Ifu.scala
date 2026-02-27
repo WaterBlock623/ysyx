25be2c27b4db8c07e2c33a0a384389a0670ddb8b
tracer-ysyx <tracer@ysyx.org>
2026-02-27 15:04:41 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:04:41 up  5:04,  1 user,  load average: 0.17, 0.60, 0.79

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -13,7 +13,7 @@ class Ifu(
   val out = IO(Decoupled(new IfuToIduIO))
   val debug = Option.when(cfg.isDebug)(IO(Output(UInt(cfg.xlen.W))))
 
-  val outBits = Wire(chiselTypeOf(out.bits))
+  val outBits = out.bits
 
   // FSM
   import DecoupledState._
@@ -25,68 +25,22 @@ class Ifu(
     )
   )
 
+  exte.mem.reqValid := state === sBusy
 
   val queue = Module(new Queue(UInt(cfg.xlen.W), 1, flow = true))
+  // enq
   queue.io.enq.valid := exte.mem.respValid
   queue.io.enq.bits := exte.mem.rData
   exte.mem.respReady := queue.io.enq.ready
+  // deq
   out.valid := queue.io.deq.valid
   outBits.ifuPayload.ifu.inst := queue.io.deq.bits
   queue.io.deq.ready := out.ready
   
-  out.bits := outBits
-
-  // val delay_cyc = 10
-  // val test_cnt = RegInit(delay_cyc.U(8.W))
-  // when (test_cnt =/= 0.U) {
-  //   test_cnt := test_cnt - 1.U
-  // }
-  // when (state === sWait && out.fire) {
-  //   test_cnt := delay_cyc.U
-  // }
-  exte.mem.reqValid := state === sBusy // && test_cnt === 0.U
-
-  // out.bits := Mux(
-  //   exte.mem.respValid,
-  //   outBits,
-  //   RegEnable(outBits, state === sBusy && exte.mem.respValid)
-  // )
-  // out.bits := RegEnable(outBits, state === sBusy && exte.mem.respValid)
-  // out.valid := state === sWait
-  // exte.mem.reqValid := !reset.asBool && (state === sIdle || (state === sWait && out.ready))
-  // val isSBusy = state === sBusy
-  // exte.mem.reqValid := isSBusy && !RegNext(isSBusy)
-  // exte.mem.reqValid := !RegNext(reset.asBool) && (state === sBusy && !exte.mem.respValid)
-
-  //
-  // import chisel3.util.random.LFSR
-  //
-  // val lfsr = LFSR(4)
-  // val delayReg = RegInit(0.U(4.W))
-  // val isNewReq = state === sWait && !RegNext(state === sWait)
-  //
-  // when(isNewReq) {
-  //   delayReg := lfsr
-  // }.elsewhen(delayReg > 0.U) {
-  //   delayReg := delayReg - 1.U
-  // }
-  //
-  // state := MuxLookup(state, sBusy)(
-  //   Seq(
-  //     // sIdle -> Mux(out.ready, sBusy, sIdle),
-  //     sBusy -> sWait,
-  //     sWait -> Mux(out.fire, sBusy, sWait)
-  //   )
-  // )
-  //
-  // out.valid := state === sWait && delayReg === 0.U && !isNewReq
-  //
-
+  // pc
   val pc = exte.pcReg.pc
   exte.mem.rAddr := pc
-  val inst = exte.mem.rData
   outBits.ifuPayload.ifu.pc := exte.pcReg.pc
-  // outBits.ifuPayload.ifu.inst := inst
 
   if (cfg.isDebug) {
     debug.get := out.bits.ifuPayload.ifu.inst
