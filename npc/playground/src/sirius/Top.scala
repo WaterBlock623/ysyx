e7abad5d8c11d7e44d1226c30ca82e422ab5723c
tracer-ysyx <tracer@ysyx.org>
2026-02-28 20:37:33 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:37:33 up  9:25,  1 user,  load average: 3.65, 1.87, 0.88

--- npc/playground/src/sirius/Top.scala
+++ npc/playground/src/sirius/Top.scala
@@ -411,7 +411,7 @@ class MemDpiC(
   private val maskMsb = (cfg.xlen >> 3) - 1
   private val maskZero = 32 - (cfg.xlen >> 3)
 
-  private val delayProb = 0
+  private val delayProb = 100
   private val maxDelayCycle = 30
   
   setInline(
