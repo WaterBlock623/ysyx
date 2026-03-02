c88b81f09587d080b0c37d59da52d9178aa2866a
tracer-ysyx <tracer@ysyx.org>
2026-03-02 20:51:10 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:51:10 up  9:28,  1 user,  load average: 0.48, 0.72, 0.73

--- npc/playground/src/sirius/Top.scala
+++ npc/playground/src/sirius/Top.scala
@@ -465,7 +465,7 @@ always @(posedge clock) begin
       end else if (AXI_ARVALID && AXI_ARREADY) begin
         read_state <= 1;
         /* verilator lint_off UNSIGNED */
-        read_delay_cnt <= ($$urandom_range(0, 100) < ${100-delayProb}) ? 0 : $$urandom_range(1, ${maxDelayCycle});
+        read_delay_cnt <= ($$urandom_range(0, 99) < ${100-delayProb}) ? 0 : $$urandom_range(1, ${maxDelayCycle});
         internal_ls_rData <= dpic_pmem_read(AXI_ARADDR);
       end
     end else begin
@@ -474,7 +474,7 @@ always @(posedge clock) begin
       end else if (AXI_RVALID && AXI_RREADY) begin
         read_state <= 0;
         /* verilator lint_off UNSIGNED */
-        read_delay_cnt <= ($$urandom_range(0, 100) < ${100-delayProb}) ? 0 : $$urandom_range(1, ${maxDelayCycle});
+        read_delay_cnt <= ($$urandom_range(0, 99) < ${100-delayProb}) ? 0 : $$urandom_range(1, ${maxDelayCycle});
       end
     end
   end
@@ -499,7 +499,7 @@ always @(posedge clock) begin
       end else if (AXI_AWVALID && AXI_WVALID) begin
         write_state <= 1;
         /* verilator lint_off UNSIGNED */
-        write_delay_cnt <= ($$urandom_range(0, 100) < ${100-delayProb}) ? 0 : $$urandom_range(1, ${maxDelayCycle});
+        write_delay_cnt <= ($$urandom_range(0, 99) < ${100-delayProb}) ? 0 : $$urandom_range(1, ${maxDelayCycle});
         dpic_pmem_write(AXI_AWADDR, AXI_WDATA, {${maskZero}'b0, AXI_WSTRB});
       end
     end else begin
@@ -508,7 +508,7 @@ always @(posedge clock) begin
       end else if (AXI_BVALID && AXI_BREADY) begin
         write_state <= 0;
         /* verilator lint_off UNSIGNED */
-        write_delay_cnt <= ($$urandom_range(0, 100) < ${100-delayProb}) ? 0 : $$urandom_range(1, ${maxDelayCycle});
+        write_delay_cnt <= ($$urandom_range(0, 99) < ${100-delayProb}) ? 0 : $$urandom_range(1, ${maxDelayCycle});
       end
     end
   end
