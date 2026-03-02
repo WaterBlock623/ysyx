2236a4c02e89676269dc2ee2723ee402463f8362
tracer-ysyx <tracer@ysyx.org>
2026-03-02 18:27:45 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:27:45 up  7:05,  1 user,  load average: 1.68, 1.39, 0.79

--- npc/playground/src/sirius/Xbar.scala
+++ npc/playground/src/sirius/Xbar.scala
@@ -104,6 +104,8 @@ class UartDevice extends Module {
   in.aw.ready := inputValid
   in.w.ready := inputValid
   in.b.valid := state === sWaitResp
+  in.b.bits.resp := 0.U
+  in.b.bits.data := 0.U
   when(inputValid) {
     printf("[sim] %c\n", in.w.bits.data(7, 0))
   }
