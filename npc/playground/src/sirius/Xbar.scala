be7d742e879371cd52fd7e12433d4708d47371ad
tracer-ysyx <tracer@ysyx.org>
2026-03-03 13:58:07 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:58:07 up  1:38,  1 user,  load average: 1.71, 1.11, 1.19

--- npc/playground/src/sirius/Xbar.scala
+++ npc/playground/src/sirius/Xbar.scala
@@ -156,9 +156,8 @@ class UartDevice extends Module {
   in.b.valid := state === sWaitResp
   in.b.bits.resp := 0.U
 
-  val esc = 27.toChar
   when(inputValid) {
-    printf(s"${esc}[34m%c${esc}[0m", in.w.bits.data(7, 0))
+    printf("%c", in.w.bits.data(7, 0))
   }
 }
 
