58d87b32867d09f10c2ef540ae85f7939158a3c4
tracer-ysyx <tracer@ysyx.org>
2026-03-05 18:23:38 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:23:38 up  7:07,  1 user,  load average: 0.08, 0.13, 0.09

--- abstract-machine/am/src/riscv/npc-ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/npc-ysyxsoc/trm.c
@@ -4,14 +4,10 @@
 #include <riscv/riscv.h>
 #include "npc.h"
 
-extern char _heap_start;
 int main(const char *args);
 
-extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
-#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
-
-Area heap = RANGE(&_heap_start, PMEM_END);
+extern char _heap_start, _heap_end;
+Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
