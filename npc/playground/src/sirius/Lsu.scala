0029d4ccaa7beb592c6c4eaee3ba7f7bc8804e62
tracer-ysyx <tracer@ysyx.org>
2026-03-05 23:42:09 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:42:09 up 12:38,  1 user,  load average: 0.71, 0.28, 0.36

--- npc/playground/src/sirius/Lsu.scala
+++ npc/playground/src/sirius/Lsu.scala
@@ -20,7 +20,7 @@ class Lsu(
   val inBits = in.bits
   val outBits = out.bits
   val ctrl = inBits.ctrl.lsuCtrl
-  val addr = inBits.exuPayload.exu.aluOut & ~"b11".U(cfg.xlen.W)
+  val addr = inBits.exuPayload.exu.aluOut
 
   // 数据透传
   outBits.lsuPayload.viewAsSupertype(new ExuPayload) := inBits.exuPayload
