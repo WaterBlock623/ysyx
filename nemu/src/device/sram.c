e329447607258acb5368da2cf1006e646dcd0b39
tracer-ysyx <tracer@ysyx.org>
2026-03-05 20:15:21 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:15:21 up  9:08,  1 user,  load average: 0.22, 0.16, 0.06

--- nemu/src/device/sram.c
+++ nemu/src/device/sram.c
@@ -3,11 +3,11 @@
 
 static uint8_t *sram_base = NULL;
 
-static inline void mrom_io_handler(uint32_t offset, int len, bool is_write) {}
+static inline void sram_io_handler(uint32_t offset, int len, bool is_write) {}
 
-void init_mrom(device_init_param_t *param) {
+void init_sram(device_init_param_t *param) {
   sram_base = new_space(CONFIG_SRAM_SIZE);
-  IOMap *map = add_mmio_map("sram", CONFIG_SRAM_MMIO, mrom_base, CONFIG_SRAM_SIZE, mrom_io_handler, false);
+  IOMap *map = add_mmio_map("sram", CONFIG_SRAM_MMIO, sram_base, CONFIG_SRAM_SIZE, sram_io_handler, false);
   if (param->sram_img) {
     device_load_img(map, param->sram_img);
   }
