9a70a9dd83acff7a2495d2a9bcce71095464c053
tracer-ysyx <tracer@ysyx.org>
2026-03-05 20:27:59 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:27:59 up  9:22,  1 user,  load average: 0.03, 0.14, 0.16

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -130,12 +130,14 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   if (is_skip_ref) {
     // to skip the checking of an instruction, just copy the reg state to
     // reference design
+    printf("skip!\n");
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
     is_skip_ref = false;
     return;
   }
   ref_difftest_exec(1);
   if (is_skip_ref) {
+    printf("skip!\n");
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
     is_skip_ref = false;
     return;
