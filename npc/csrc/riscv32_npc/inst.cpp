0f021366c5094cdd714101a0d913fbcdabe9da77
tracer-ysyx <tracer@ysyx.org>
2026-03-05 21:49:41 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:49:41 up 10:44,  1 user,  load average: 2.33, 0.85, 0.39

--- npc/csrc/riscv32_npc/inst.cpp
+++ npc/csrc/riscv32_npc/inst.cpp
@@ -206,7 +206,7 @@ int isa_exec_once(Decode *s) {
   } else {
     s->dnpc = npc_dnpc;
     IFDEF(CONFIG_ITRACE, print_disassemble(s));
-    printf("%x\n", gpr(2));
+    // printf("%x\n", gpr(2));
   }
 
   decode_inst(s);
