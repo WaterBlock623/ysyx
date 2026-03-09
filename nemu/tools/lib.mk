65ce3e91a91bc92ec6346f03a81786bec9fbbef1
tracer-ysyx <tracer@ysyx.org>
2026-03-09 13:36:44 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:36:44 up  3:21,  1 user,  load average: 0.08, 0.03, 1.04

--- nemu/tools/lib.mk
+++ nemu/tools/lib.mk
@@ -4,7 +4,7 @@ STATIC_LIBS = $(addprefix $(BUILD_DIR)/,$(_STATIC_LIBS))
 TOOLS_DIR = $(NEMU_HOME)/tools
 
 mini-gdbstub:
-	$(MAKE) -C $(TOOLS_DIR)/mini-gdbstub all O=$(BUILD_DIR) LIBGDBSTUB=$(BUILD_DIR)/libgdbstub.a
+	$(MAKE) -C $(TOOLS_DIR)/mini-gdbstub all O=$(BUILD_DIR)/mini-gdbstub LIBGDBSTUB=$(BUILD_DIR)/libgdbstub.a
 
 $(STATIC_LIBS): mini-gdbstub
 
