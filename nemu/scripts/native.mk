b6397860350edeea18786ea28c3ad98c6eed990e
tracer-ysyx <tracer@ysyx.org>
2026-03-10 10:52:26 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:52:26 up 23 min,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -37,7 +37,8 @@ $(info NEMU BUILD_DIR $(BUILD_DIR))
 IMG ?=
 # NEMU_EXEC := numactl -m 0 -C 0,2,4,6 -- $(BINARY) $(ARGS) $(IMG)
 ifeq ($(CONFIG_DEBUGER_GDB),y)
-NEMU_EXEC := ($(BINARY) $(ARGS) $(IMG) &) && riscv64-unknown-linux-gnu-gdb
+# NEMU_EXEC := ($(BINARY) $(ARGS) $(IMG) &) && riscv64-unknown-linux-gnu-gdb
+NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 else
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 endif
