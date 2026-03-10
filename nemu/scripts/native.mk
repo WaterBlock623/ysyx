54d3351c53800654cdcafd3a47068b978a1b784a
tracer-ysyx <tracer@ysyx.org>
2026-03-10 17:04:30 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:04:30 up  6:16,  1 user,  load average: 0.09, 0.09, 0.03

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -42,7 +42,7 @@ ifeq ($(CONFIG_DEBUGER_GDB),y)
 $(info GDB_SOCKET $(GDB_SOCKET))
 NEMU_EXEC := ($(_NEMU_EXEC) &) \
 						 && (if ! echo "$(GDB_SOCKET)" | grep -q ":" && \
-						 [ ! -e "$(GDB_SOCKET)" ]; then echo "WAIT"; sleep 5; fi; \
+						 [ ! -e "$(GDB_SOCKET)" ]; then sleep 0.5; fi; \
 						 riscv64-unknown-linux-gnu-gdb -ex "target remote $(GDB_SOCKET)")
 else
 NEMU_EXEC := $(_NEMU_EXEC)
