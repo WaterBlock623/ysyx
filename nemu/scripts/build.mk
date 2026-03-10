632a4bc3dea7f0d708b29506b3c2b22036f8b57b
tracer-ysyx <tracer@ysyx.org>
2026-03-10 20:59:58 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:59:58 up 10:06,  1 user,  load average: 0.04, 0.11, 0.11

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -53,8 +53,6 @@ app: $(BINARY)
 
 $(info NEMU archives $(ARCHIVES))
 $(BINARY):: $(OBJS) $(ARCHIVES) $(BINARY_DEPS)
-	@echo "目标: $@"
-	@echo "触发更新的文件是: $?"
 	@echo + LD $@
 	@$(LD) -o $@ $(OBJS) $(LDFLAGS) $(ARCHIVES) $(LIBS)
 
