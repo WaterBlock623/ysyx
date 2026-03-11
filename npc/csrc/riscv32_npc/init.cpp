ab8a7f2b75cf80e54abc23075f90fc216cbf9547
tracer-ysyx <tracer@ysyx.org>
2026-03-11 21:46:55 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:46:55 up 10:38,  1 user,  load average: 0.27, 0.28, 0.23

--- npc/csrc/riscv32_npc/init.cpp
+++ npc/csrc/riscv32_npc/init.cpp
@@ -39,7 +39,7 @@ int npc_wbu_valid = 0;
 extern "C" void flash_read(uint32_t addr, int32_t *data) { 
   printf("MMIO %x\n", CONFIG_FLASH_MMIO);
   printf("1%x\n", addr);
-  // addr += CONFIG_FLASH_MMIO;
+  addr += CONFIG_FLASH_MMIO;
   IFDEF(CONFIG_FLASH_MMIO, addr += CONFIG_FLASH_MMIO);
   printf("2%x\n", addr);
   addr &= ~3u;
