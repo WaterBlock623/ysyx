47af3379f5b0c3e8ce0ed554ee4b77d44f57f027
tracer-ysyx <tracer@ysyx.org>
2026-03-11 15:35:54 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:35:54 up  4:27,  1 user,  load average: 0.13, 0.09, 0.07

--- abstract-machine/am/src/riscv/npc-ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/npc-ysyxsoc/trm.c
@@ -26,6 +26,22 @@ static inline void put_csrid(void) {
   // printf("[TRM] mvendorid: 0x%llx  marchid: %llu\n", mvendorid, marchid);
 }
 
+static void serial_init(void) {
+#define SERIAL_FREQ 50 * 1000000
+#define SERIAL_BAUD 115200
+#define SERIAL_DL_VAL ((uint16_t)((SERIAL_FREQ) / (16 * (SERIAL_BAUD))))
+
+#define SERIAL_DLLO (SERIAL_PORT)
+#define SERIAL_DLHI (SERIAL_PORT + 1u)
+#define SERIAL_LCR (SERIAL_PORT + 3u)
+#define SERIAL_LSR (SERIAL_PORT + 5u)
+
+  setb(SERIAL_LCR, 1u << 7);
+  outb(SERIAL_DLHI, (uint8_t)(SERIAL_DL_VAL >> 8));
+  outb(SERIAL_DLLO, (uint8_t)SERIAL_DL_VAL);
+  clearb(SERIAL_LCR, 1u << 7);
+}
+
 extern void __am_asm_trap(void);
 extern char _data_load_start[], _data_start[], _data_end[], _data_size[];
 extern char _bss_start[], _bss_end[], _bss_size[];
@@ -33,6 +49,8 @@ void _trm_init() {
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
   memcpy(_data_start, _data_load_start, (size_t)_data_size); 
   memset(_bss_start, 0, (size_t)_bss_size);
+
+  serial_init();
 
   // put_csrid();
   int ret = main(mainargs);
