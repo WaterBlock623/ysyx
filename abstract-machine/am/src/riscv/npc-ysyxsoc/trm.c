709b1c800db4ffacf3bdffc5b9ab9bcec4a42026
tracer-ysyx <tracer@ysyx.org>
2026-03-13 18:01:54 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:01:54 up  7:06,  1 user,  load average: 0.17, 0.64, 0.65

--- abstract-machine/am/src/riscv/npc-ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/npc-ysyxsoc/trm.c
@@ -21,11 +21,11 @@ void halt(int code) {
   while (1);
 }
 
-static inline void put_csrid(void) {
+static void put_csrid(void) {
   unsigned long long mvendorid = 0, marchid = 0; 
   asm volatile("csrr %0, mvendorid" : "=r"(mvendorid));
   asm volatile("csrr %0, marchid" : "=r"(marchid));
-  // printf("[TRM] mvendorid: 0x%llx  marchid: %llu\n", mvendorid, marchid);
+  printf("[TRM] mvendorid: 0x%llx  marchid: %llu\n", mvendorid, marchid);
 }
 
 static void serial_init(void) {
@@ -53,7 +53,7 @@ void _trm_init() {
 
   serial_init();
 
-  // put_csrid();
+  put_csrid();
   int ret = main(mainargs);
   halt(ret);
 }
