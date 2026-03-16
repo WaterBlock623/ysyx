f230f9c707cdeeffd1df9a90d8d037256dbe812d
tracer-ysyx <tracer@ysyx.org>
2026-03-16 14:58:49 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:58:49 up  4:45,  1 user,  load average: 0.22, 0.16, 0.17

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -8,6 +8,7 @@ LDFLAGS += -shared -fPIC
 endif
 
 INC_PATH := $(ADD_INC_PATH) $(WORK_DIR)/include $(NEMU_HOME)/include $(INC_PATH)
+INC_PATH := $(abspath $(INC_PATH))
 # ifneq ($(CONFIG_NPC),)
 # INC_PATH := $(ADD_INC_PATH) $(INC_PATH)
 # endif
@@ -23,7 +24,7 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -O2 -MMD -Wall -Werror $(abspath $(INCLUDES)) $(CFLAGS)
+CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
 LDFLAGS := -O2 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
