f4633e95d737a6dfaf844d0846de93e6468e140a
tracer-ysyx <tracer@ysyx.org>
2026-03-17 12:19:04 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:19:04 up  1:15,  1 user,  load average: 0.12, 0.07, 0.07

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -74,7 +74,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
 }
 
 bool try_map_read(paddr_t addr, int len, IOMap *map, word_t *dest) {
-  if (len < 1 || len > 8 || !in_mmio(map, addr)) {
+  if (len < 1 || len > 4 || !in_mmio(map, addr)) {
     return false;
   }
   paddr_t offset = addr - map->low;
@@ -84,7 +84,7 @@ bool try_map_read(paddr_t addr, int len, IOMap *map, word_t *dest) {
 }
 
 bool try_map_write(paddr_t addr, int len, word_t *data, IOMap *map) {
-  if (len < 1 || len > 8 || !in_mmio(map, addr)) {
+  if (len < 1 || len > 4 || !in_mmio(map, addr)) {
     return false;
   }
   paddr_t offset = addr - map->low;
