6f8577bd97aed7659fc146b0dcdb36fda882f458
tracer-ysyx <tracer@ysyx.org>
2026-03-18 00:52:01 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:52:01 up  5:00,  1 user,  load average: 0.39, 0.84, 0.78

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -31,7 +31,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INST_TO_PRINT 10
+#define MAX_INST_TO_PRINT 0
 
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
