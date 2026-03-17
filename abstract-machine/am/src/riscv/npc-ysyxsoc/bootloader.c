7c970cc6d78927c2fbc7ea939cb86dada6f3c6a6
tracer-ysyx <tracer@ysyx.org>
2026-03-17 13:26:12 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:26:12 up  2:22,  1 user,  load average: 0.00, 0.03, 0.04

--- abstract-machine/am/src/riscv/npc-ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/npc-ysyxsoc/bootloader.c
@@ -8,7 +8,9 @@ extern char _bss_start[], _bss_end[], _bss_size[];
 
 void _trm_init(void);
 
-void __attribute__((section(".bootloader"))) _bootloader(void) {
+__attribute__((section(".bootloader")))
+__attribute__((flatten))
+void  _bootloader(void) {
   memcpy(_data_start, _data_load_start, (size_t)_data_size); 
   memset(_bss_start, 0, (size_t)_bss_size);
 
