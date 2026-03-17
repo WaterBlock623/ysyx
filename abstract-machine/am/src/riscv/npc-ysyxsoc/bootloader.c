3ed8694f42844017d790a2212b7dc13195a93052
tracer-ysyx <tracer@ysyx.org>
2026-03-17 13:33:06 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:33:06 up  2:29,  1 user,  load average: 0.18, 0.08, 0.05

--- abstract-machine/am/src/riscv/npc-ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/npc-ysyxsoc/bootloader.c
@@ -3,6 +3,30 @@
 #include <klib.h>
 #include <riscv/riscv.h>
 
+static __attribute__((always_inline)) inline
+void *_memset(void *s, int c, size_t n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    ((unsigned char *)s)[i] = (unsigned char)c; 
+  }  
+  return s;
+}
+
+static __attribute__((always_inline)) inline
+void *_mempcpy(void *out, const void *in, size_t n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    ((unsigned char *)out)[i] = ((unsigned char *)in)[i];
+  }
+  return (unsigned char *)out + n;
+}
+
+static __attribute__((always_inline)) inline
+void *_memcpy(void *out, const void *in, size_t n) {
+  _mempcpy(out, in, n);
+  return out;
+}
+
 extern char _data_load_start[], _data_start[], _data_end[], _data_size[];
 extern char _bss_start[], _bss_end[], _bss_size[];
 
@@ -11,8 +35,8 @@ void _trm_init(void);
 __attribute__((section(".bootloader")))
 __attribute__((flatten))
 void  _bootloader(void) {
-  memcpy(_data_start, _data_load_start, (size_t)_data_size); 
-  memset(_bss_start, 0, (size_t)_bss_size);
+  _memcpy(_data_start, _data_load_start, (size_t)_data_size); 
+  _memset(_bss_start, 0, (size_t)_bss_size);
 
   _trm_init();
 }
