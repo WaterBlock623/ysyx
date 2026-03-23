faf78f39a871c5e2f51801f80291bd28ee639fb0
tracer-ysyx <tracer@ysyx.org>
2026-03-23 20:22:33 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:22:33 up  8:33,  1 user,  load average: 0.01, 0.22, 0.50

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -42,7 +42,9 @@ endif
 # CROSS_GDB = riscv64-unknown-linux-gnu-gdb
 CROSS_GDB = riscv64-unknown-elf-gdb
 GDB_FLAGS += -ex "set can-use-hw-watchpoints 0" \
-						 -ex "target remote $(GDB_SOCKET)"
+						 -ex "source $(NEMU_HOME)/tools/gdb-scripts/smart-connect.py" \
+						 -ex "smart-connect $(GDB_SOCKET)"
+						 # -ex "target remote $(GDB_SOCKET)"
 GDB_FLAGS += $(AM_GDB_FLAGS)
 NEMU_EXEC := $(_NEMU_EXEC) & \
     NEMU_PID=$$!; \
