a8ad6d3af5df688271dd6aea0976506a6834227e
tracer-ysyx <tracer@ysyx.org>
2026-03-24 20:32:09 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:32:09 up 1 day,  8:42,  1 user,  load average: 0.12, 0.13, 0.10

--- abstract-machine/am/src/riscv/npc-ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/npc-ysyxsoc/trm.c
@@ -32,7 +32,7 @@ static void serial_init(void) {
 // #define SERIAL_FREQ 50 * 1000000
 // #define SERIAL_BAUD 115200
 // #define SERIAL_DL_VAL ((uint16_t)((SERIAL_FREQ) / (16 * (SERIAL_BAUD))))
-#define SERIAL_DL_VAL (uint16_t)4u
+#define SERIAL_DL_VAL (uint16_t)1u
 
 #define SERIAL_DLLO (SERIAL_PORT)
 #define SERIAL_DLHI (SERIAL_PORT + 1u)
