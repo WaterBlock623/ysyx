7b2d245527645dff51468b4edf150752283239e6
tracer-ysyx <tracer@ysyx.org>
2026-03-24 16:59:19 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:59:19 up 1 day,  5:09,  1 user,  load average: 0.13, 0.12, 0.09

--- npc/csrc/riscv32_npc/reg.c
+++ npc/csrc/riscv32_npc/reg.c
@@ -60,11 +60,8 @@ bool isa_try_read_reg(int regno, void *dest) {
   bool success = isa_try_find_reg(regno, &reg, &len);
   if (success) {
     memcpy(dest, reg, len);
-  } else {
-    memset(dest, 0, len);
   }
-  // return success;
-  return true;
+  return success;
 }
 
 bool isa_try_write_reg(int regno, const void *src) {
