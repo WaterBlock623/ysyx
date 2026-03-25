a77acb1de9655ef2d30206b21d42d39c8a9753ab
tracer-ysyx <tracer@ysyx.org>
2026-03-25 15:27:34 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:27:34 up  5:06,  1 user,  load average: 0.00, 0.04, 0.06

--- abstract-machine/am/src/riscv/npc-ysyxsoc/ioe/input.c
+++ abstract-machine/am/src/riscv/npc-ysyxsoc/ioe/input.c
@@ -2,18 +2,48 @@
 #include "../npc.h"
 
 #define SCAN_KEYS(_) \
-  _(ESCAPE, 0x76) _(F1, 0x05) _(F2, 0x06) _(F3) _(F4) _(F5) _(F6) _(F7) _(F8) _(F9) _(F10) _(F11) _(F12) \
-  _(GRAVE) _(1) _(2) _(3) _(4) _(5) _(6) _(7) _(8) _(9) _(0) _(MINUS) _(EQUALS) _(BACKSPACE) \
-  _(TAB) _(Q) _(W) _(E) _(R) _(T) _(Y) _(U) _(I) _(O) _(P) _(LEFTBRACKET) _(RIGHTBRACKET) _(BACKSLASH) \
-  _(CAPSLOCK) _(A) _(S) _(D) _(F) _(G) _(H) _(J) _(K) _(L) _(SEMICOLON) _(APOSTROPHE) _(RETURN) \
-  _(LSHIFT) _(Z) _(X) _(C) _(V) _(B) _(N) _(M) _(COMMA) _(PERIOD) _(SLASH) _(RSHIFT) \
-  _(LCTRL) _(APPLICATION) _(LALT) _(SPACE) _(RALT) _(RCTRL) \
-  _(UP) _(DOWN) _(LEFT) _(RIGHT) _(INSERT) _(DELETE) _(HOME) _(END) _(PAGEUP) _(PAGEDOWN)
+  _(ESCAPE, 0x76) _(F1, 0x05) _(F2, 0x06) _(F3, 0x04) _(F4, 0x0C) _(F5, 0x03) _(F6, 0x0B) _(F7, 0x83) _(F8, 0x0A) _(F9, 0x01) _(F10, 0x09) _(F11, 0x78) _(F12, 0x07) \
+  _(GRAVE, 0x0E) _(1, 0x16) _(2, 0x1E) _(3, 0x26) _(4, 0x25) _(5, 0x2E) _(6, 0x36) _(7, 0x3D) _(8, 0x3E) _(9, 0x46) _(0, 0x45) _(MINUS, 0x4E) _(EQUALS, 0x55) _(BACKSPACE, 0x66) \
+  _(TAB, 0x0D) _(Q, 0x15) _(W, 0x1D) _(E, 0x24) _(R, 0x2D) _(T, 0x2C) _(Y, 0x35) _(U, 0x3C) _(I, 0x43) _(O, 0x44) _(P, 0x4D) _(LEFTBRACKET, 0x54) _(RIGHTBRACKET, 0x5B) _(BACKSLASH, 0x5D) \
+  _(CAPSLOCK, 0x58) _(A, 0x1C) _(S, 0x1B) _(D, 0x23) _(F, 0x2B) _(G, 0x34) _(H, 0x33) _(J, 0x3B) _(K, 0x42) _(L, 0x4B) _(SEMICOLON, 0x4C) _(APOSTROPHE, 0x52) _(RETURN, 0x5A) \
+  _(LSHIFT, 0x12) _(Z, 0x1A) _(X, 0x22) _(C, 0x21) _(V, 0x2A) _(B, 0x32) _(N, 0x31) _(M, 0x3A) _(COMMA, 0x41) _(PERIOD, 0x49) _(SLASH, 0x4A) _(RSHIFT, 0x59) \
+  _(LCTRL, 0x14) _(LALT, 0x11) _(SPACE, 0x29)
 
 #define SCAN_EXT_KEYS(_) \
-  _(RALT, 0x11)
+  _(RALT, 0x11) _(RCTRL, 0x14) \
+  _(UP, 0x75) _(DOWN, 0x72) _(LEFT, 0x6B) _(RIGHT, 0x74) \
+  _(INSERT, 0x70) _(DELETE, 0x71) _(HOME, 0x6C) _(END, 0x69) _(PAGEUP, 0x7D) _(PAGEDOWN, 0x7A)
+
+#define KEY_MAP(name, scan) [scan] = AM_KEY_NAMES(name)
+
+static uint8_t keycode[] = {
+  SCAN_KEYS(KEY_MAP)
+};
+
+static uint8_t keycode_ext[] = {
+  SCAN_EXT_KEYS(KEY_MAP)
+};
+
+static void get_keybrd(AM_INPUT_KEYBRD_T *kbd, bool is_ext, bool is_down) {
+  uint8_t scan_code = inb(KBD_ADDR);
+  switch (scan_code) {
+    case 0x0:
+      kbd->keydown = false;
+      kbd->keycode = AM_KEY_NONE;
+      break;
+    case 0xE0:
+      get_keybrd(kbd, true, is_down);
+      break;
+    case 0xF0:
+      get_keybrd(kbd, is_ext, false);
+      break;
+    default:
+      kbd->keydown = is_down;
+      kbd->keycode = is_ext ? keycode_ext[scan_code] : keycode[scan_code];
+      break;
+  }
+}
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  // uint8_t scan_code = inb(KBD_ADDR);
-  
+  get_keybrd(kbd, false, true); 
 }
