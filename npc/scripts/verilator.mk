e9f88b2867d9481b2368184a4e38c270648d15ea
tracer-ysyx <tracer@ysyx.org>
2026-03-25 20:22:43 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:22:43 up  9:56,  1 user,  load average: 0.76, 0.81, 0.75

--- npc/scripts/verilator.mk
+++ npc/scripts/verilator.mk
@@ -23,7 +23,7 @@ else
 $(info profile.vlt not found, skipping PGO)
 endif
 
-VERILATOR_BUILDFLAGS += -MMD --cc --build -j 16 --autoflush --threads 1 \
+VERILATOR_BUILDFLAGS += -MMD --cc --build -j 16 --autoflush --threads 16 --prof-exec \
 				-O3 --x-assign fast --x-initial fast --noassert --threads 1
 VERILATOR_FLAGS += $(addprefix -y , $(YSYXSOC_LIBDIR))
 VERILATOR_FLAGS += --timescale "1ns/1ns" --no-timing
