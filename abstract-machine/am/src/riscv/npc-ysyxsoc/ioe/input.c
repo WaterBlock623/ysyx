3d29d5bfa256e7b9a78d08c6874c1e0726a510ad
tracer-ysyx <tracer@ysyx.org>
2026-03-25 15:38:47 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:38:47 up  5:17,  1 user,  load average: 0.58, 0.60, 0.39

--- abstract-machine/am/src/riscv/npc-ysyxsoc/ioe/input.c
+++ abstract-machine/am/src/riscv/npc-ysyxsoc/ioe/input.c
@@ -26,8 +26,8 @@ static uint8_t keycode_ext[] = {
 };
 
 static void get_keybrd(AM_INPUT_KEYBRD_T *kbd, bool is_ext, bool is_down) {
-  volatile uint8_t scan_code = inb(KBD_ADDR);
-  // if (scan_code) printf("# 0x%x\n", scan_code);
+  uint8_t scan_code = inb(KBD_ADDR);
+  if (scan_code) printf("# 0x%x\n", scan_code);
   switch (scan_code) {
     case 0x0:
       kbd->keydown = false;
