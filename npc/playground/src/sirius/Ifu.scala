7165b1199ded56cd1ed8e5560dab96063ef7e962
tracer-ysyx <tracer@ysyx.org>
2026-03-27 16:25:57 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:25:57 up  4:57,  1 user,  load average: 0.73, 1.29, 1.09

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -37,7 +37,8 @@ class Ifu(
     )
   )
 
-  out.valid := RegNext((state === sWaitResp && exte.mem.r.valid) || state === sKeepData)
+  // out.valid := (state === sWaitResp && exte.mem.r.valid) || state === sKeepData
+  out.valid := state === sKeepData
   val dataReg = RegEnable(exte.mem.r.bits.data, state === sWaitResp && exte.mem.r.valid && !out.fire)
   // outBits.ifuPayload.ifu.inst := Mux(state === sKeepData, dataReg, exte.mem.r.bits.data)
   outBits.ifuPayload.ifu.inst := dataReg
