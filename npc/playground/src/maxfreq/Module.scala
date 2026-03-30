e94658472a473d24b7291a482e32585ab98102bd
tracer-ysyx <tracer@ysyx.org>
2026-03-30 17:27:20 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:27:20 up  6:28,  1 user,  load average: 0.20, 0.33, 0.27

--- npc/playground/src/maxfreq/Module.scala
+++ npc/playground/src/maxfreq/Module.scala
@@ -3,27 +3,28 @@ package maxfreq
 import chisel3._
 
 class RegFile extends Module {
-  val inRaw = IO(new Bundle {
+  val io = IO(new Bundle {
     val rAddr = Input(Vec(2, UInt(4.W)))
+    val rData = Output(Vec(2, UInt(32.W)))
     val wEn   = Input(Bool())
     val wAddr = Input(UInt(4.W))
     val wData = Input(UInt(32.W))
   })
-  val outRaw = IO(new Bundle {
-    val rData = Output(Vec(2, UInt(32.W)))
-  })
 
-  val in = RegNext(inRaw) 
-  val out = Wire(chiselTypeOf(outRaw))
-  outRaw := RegNext(out)
+  val rAddr = RegNext(io.rAddr)
+  val rData = Wire(chiselTypeOf(io.rData))
+  io.rData := RegNext(rData)
+  val wEn   = RegNext(io.wEn)
+  val wAddr = RegNext(io.wAddr)
+  val wData = RegNext(io.wData)
 
   val regFile = Reg(Vec(16, UInt(32.W)))
-  when(in.wEn) {
-    regFile(in.wAddr) := in.wData
+  when(wEn) {
+    regFile(wAddr) := wData
   }
   regFile(0) := 0.U
-  out.rData(0) := regFile(in.rAddr(0))
-  out.rData(1) := regFile(in.rAddr(1))
+  rData(0) := regFile(rAddr(0))
+  rData(1) := regFile(rAddr(1))
 }
 
 class Adder extends Module {
