0732c07f72b777fbe4bb2ee53b60037ebf6472e7
tracer-ysyx <tracer@ysyx.org>
2026-03-30 12:27:36 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:27:36 up  1:40,  1 user,  load average: 0.89, 0.68, 0.56

--- npc/playground/src/sirius/Perf.scala
+++ npc/playground/src/sirius/Perf.scala
@@ -17,7 +17,7 @@ object PerfWhen {
     if (cfg.perf) {
       val eventCntReg = CntWhen(cntCond, width)
       when (printCond) {
-        printf("[perf %35m] " + name.padTo(15, ' ') + " = %d\n", eventCntReg)
+        printf("[perf %m] " + name.padTo(15, ' ') + " = %d\n", eventCntReg)
       } 
       Some(eventCntReg)
     } else {
