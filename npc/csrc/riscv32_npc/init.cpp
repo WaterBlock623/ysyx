b9fa43da67cb84e5ff3f58507991fb32e7c0d62c
tracer-ysyx <tracer@ysyx.org>
2026-03-30 19:55:49 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:55:49 up  8:50,  1 user,  load average: 0.22, 0.30, 0.23

--- npc/csrc/riscv32_npc/init.cpp
+++ npc/csrc/riscv32_npc/init.cpp
@@ -222,8 +222,10 @@ void single_cycle(void) {
 
 static void reset(int n) {
   top->reset = 1;
-  while (n-- > 0)
+  while (n-- > 0) {
+    printf("%d\n", n);
     single_cycle();
+  }
   top->reset = 0;
 }
 
