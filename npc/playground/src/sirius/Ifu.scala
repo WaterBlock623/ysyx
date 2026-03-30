dea5ccbed5df6a50f1a2059b389cd99e711455a9
tracer-ysyx <tracer@ysyx.org>
2026-03-30 11:47:13 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:47:13 up  1:01,  1 user,  load average: 2.44, 0.95, 0.63

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -52,6 +52,5 @@ class Ifu(
   if (cfg.isDebug) {
     debug.get := outBits.ifuPayload.ifu.inst
   }
-  PerfWhen("instFetch", exte.mem.r.fire, (out.bits.ifuPayload.ifu.inst === "h00100073".U))
-  // PerfWhen("instFetch", exte.mem.r.fire, true.B)
+  PerfWhen("instFetch", exte.mem.r.fire, out.valid && (out.bits.ifuPayload.ifu.inst === "h00100073".U))
 }
