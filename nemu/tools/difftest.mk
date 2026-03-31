975adfc087df02c3c8f048773db369918630f37b
tracer-ysyx <tracer@ysyx.org>
2026-03-31 11:11:37 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:11:37 up 37 min,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/tools/difftest.mk
+++ nemu/tools/difftest.mk
@@ -19,6 +19,7 @@ DIFF_REF_PATH = $(NEMU_HOME)/$(call remove_quote,$(CONFIG_DIFFTEST_REF_PATH))
 DIFF_REF_SO = $(DIFF_REF_PATH)/build/$(NAME)-$(call remove_quote,$(CONFIG_DIFFTEST_REF_NAME))-so
 MKFLAGS = GUEST_ISA=$(GUEST_ISA_REF) SHARE=1 ENGINE=interpreter
 ARGS_DIFF = --diff=$(DIFF_REF_SO)
+$(info ################ $(DIFF_REF_SO))
 
 ifndef CONFIG_DIFFTEST_REF_NEMU
 $(DIFF_REF_SO):
