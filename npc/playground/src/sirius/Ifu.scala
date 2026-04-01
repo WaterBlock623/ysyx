41509d71861c63c985fb59f9ee64aa2f412343e2
tracer-ysyx <tracer@ysyx.org>
2026-04-01 18:27:35 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:27:35 up  6:35,  1 user,  load average: 0.75, 0.84, 0.72

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -3,7 +3,12 @@ package sirius
 import chisel3._
 import chisel3.util._
 
-class Icache(lineNum: BigInt, lineByte: BigInt, addrByte: BigInt) extends Module {
+class Icache(
+  lineNum:   BigInt,
+  lineByte:  BigInt,
+  addrByte:  BigInt,
+  whiteList: Option[Range] = None)
+    extends Module {
   require(lineNum > 0 && lineNum.bitCount == 1)
   require(lineByte > 0 && lineByte.bitCount == 1)
   require(lineByte == 4)
@@ -16,7 +21,9 @@ class Icache(lineNum: BigInt, lineByte: BigInt, addrByte: BigInt) extends Module
   assert(!io.cached.aw.valid && !io.cached.w.valid)
 
   class AddrLine extends Bundle {
-    val tag = UInt((addrByte * 8 - log2Ceil(lineByte) - log2Ceil(lineNum)).toInt.W)
+    val tag = UInt(
+      (addrByte * 8 - log2Ceil(lineByte) - log2Ceil(lineNum)).toInt.W
+    )
     val idx = UInt(log2Ceil(lineNum).W)
     val off = UInt(log2Ceil(lineByte).W)
   }
@@ -24,9 +31,14 @@ class Icache(lineNum: BigInt, lineByte: BigInt, addrByte: BigInt) extends Module
   val rAddr = Mux(io.cached.ar.valid, io.cached.ar.bits.addr, rAddrReg)
   rAddrReg := rAddr
   val rAddrLine = rAddr.asTypeOf(new AddrLine)
+  val inWhireList = if (whiteList.isDefined) {
+    rAddr >= whiteList.get.start.U && rAddr < whiteList.get.end.U
+  } else { true.B }
 
   class Line extends Bundle {
-    val tag = UInt((addrByte * 8 - log2Ceil(lineByte) - log2Ceil(lineNum)).toInt.W)
+    val tag = UInt(
+      (addrByte * 8 - log2Ceil(lineByte) - log2Ceil(lineNum)).toInt.W
+    )
     val data = UInt((lineByte * 8).toInt.W)
   }
   val cache = Mem(lineNum.toInt, new Line)
@@ -37,11 +49,13 @@ class Icache(lineNum: BigInt, lineByte: BigInt, addrByte: BigInt) extends Module
 
   val sIdle :: sReadCache :: sMiss :: Nil = Enum(3)
   val state = RegInit(sIdle)
-  val nextState = MuxLookup(state, sIdle)(Seq(
-    sIdle -> Mux(io.cached.ar.valid, sReadCache, sIdle),
-    sReadCache -> Mux(hit, Mux(io.cached.r.fire, sIdle, sReadCache), sMiss),
-    sMiss -> Mux(io.mem.r.fire, sIdle, sMiss)
-    ))
+  val nextState = MuxLookup(state, sIdle)(
+    Seq(
+      sIdle -> Mux(io.cached.ar.valid && inWhireList, sReadCache, sIdle),
+      sReadCache -> Mux(hit, Mux(io.cached.r.fire, sIdle, sReadCache), sMiss),
+      sMiss -> Mux(io.mem.r.fire, sIdle, sMiss)
+    )
+  )
   state := nextState
 
   0.U.asTypeOf(chiselTypeOf(io.cached)) :>= io.cached
@@ -49,12 +63,15 @@ class Icache(lineNum: BigInt, lineByte: BigInt, addrByte: BigInt) extends Module
   io.cached.ar.ready := state === sReadCache && hit
   io.cached.r.valid := state === sReadCache && hit
   io.cached.r.bits.data := line.data
-  when (state === sMiss) {
+  when(state === sMiss || (state === sIdle && !inWhireList)) {
     io.mem :<>= io.cached
   }
-  when (state === sMiss && io.mem.r.fire) {
+  when(state === sMiss && io.mem.r.fire) {
     lineValid := true.B
-    cache.write(rAddrLine.idx, (rAddrLine.tag ## io.mem.r.bits.data).asTypeOf(new Line))
+    cache.write(
+      rAddrLine.idx,
+      (rAddrLine.tag ## io.mem.r.bits.data).asTypeOf(new Line)
+    )
   }
 }
 
@@ -71,12 +88,19 @@ class Ifu(
   val outBits = out.bits
   val pc = exte.pcReg.pc
 
-  val icache = Module(new Icache(lineNum = 16, lineByte = 4, addrByte = 4))
+  val icache = Module(
+    new Icache(
+      lineNum = 16,
+      lineByte = 4,
+      addrByte = 4,
+      if (cfg.ysyxsoc) { Some(0xa0000000 until 0xc0000000) }
+      else { None }
+    )
+  )
 
   out.bits.ifuPayload.trap := 0.U.asTypeOf(
     chiselTypeOf(out.bits.ifuPayload.trap)
   )
-
 
   val sIdle :: sWaitResp :: sKeepData :: Nil = Enum(3)
   val state = RegInit(sIdle)
@@ -86,7 +110,11 @@ class Ifu(
     Seq(
       sIdle -> Mux(
         icache.io.cached.ar.fire,
-        Mux(icache.io.cached.r.valid, Mux(out.fire, sIdle, sKeepData), sWaitResp),
+        Mux(
+          icache.io.cached.r.valid,
+          Mux(out.fire, sIdle, sKeepData),
+          sWaitResp
+        ),
         sIdle
       ),
       sWaitResp -> Mux(
@@ -108,7 +136,9 @@ class Ifu(
   exte.mem :<>= icache.io.mem
   icache.io.cached :<= 0.U.asTypeOf(new Axi4IO)
   icache.io.cached.ar.bits.size := "b010".U
-  assert(!icache.io.cached.aw.valid && !icache.io.cached.w.valid && !icache.io.cached.b.valid)
+  assert(
+    !icache.io.cached.aw.valid && !icache.io.cached.w.valid && !icache.io.cached.b.valid
+  )
   icache.io.cached.ar.valid := state === sIdle && canValid
   icache.io.cached.ar.bits.addr := pc
   icache.io.cached.r.ready := true.B
@@ -121,7 +151,11 @@ class Ifu(
   // out.valid := state === sKeepData
   out.valid := icache.io.cached.r.valid || state === sKeepData
   // outBits.ifuPayload.ifu.inst := dataReg
-  outBits.ifuPayload.ifu.inst := Mux(state === sKeepData, dataReg, icache.io.cached.r.bits.data)
+  outBits.ifuPayload.ifu.inst := Mux(
+    state === sKeepData,
+    dataReg,
+    icache.io.cached.r.bits.data
+  )
   outBits.ifuPayload.ifu.pc := pc
 
   if (cfg.isDebug) {
@@ -134,10 +168,10 @@ class Ifu(
   )
   if (cfg.perf) {
     val isMemBusy = RegInit(false.B)
-    when (!isMemBusy && exte.mem.ar.valid && !exte.mem.r.valid) {
+    when(!isMemBusy && exte.mem.ar.valid && !exte.mem.r.valid) {
       isMemBusy := true.B
     }
-    when (isMemBusy && exte.mem.r.valid) {
+    when(isMemBusy && exte.mem.r.valid) {
       isMemBusy := false.B
     }
     PerfWhen(
