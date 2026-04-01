959a6eda3133a8db309bcc7635d0cd6a73b7a3fe
tracer-ysyx <tracer@ysyx.org>
2026-04-01 12:29:07 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:29:07 up 33 min,  1 user,  load average: 0.49, 0.24, 0.12

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -13,7 +13,11 @@ class Ifu(
   val out = IO(Decoupled(new IfuToIduIO))
   val debug = Option.when(cfg.isDebug)(IO(Output(UInt(cfg.xlen.W))))
 
-  out.bits.ifuPayload.trap := 0.U.asTypeOf(chiselTypeOf(out.bits.ifuPayload.trap))
+  val pc = exte.pcReg.pc
+
+  out.bits.ifuPayload.trap := 0.U.asTypeOf(
+    chiselTypeOf(out.bits.ifuPayload.trap)
+  )
 
   val outBits = out.bits
 
@@ -21,39 +25,57 @@ class Ifu(
   val state = RegInit(sIdle)
   val canValid = RegNext(RegNext(!reset.asBool))
 
-  exte.mem :<= 0.U.asTypeOf(new Axi4IO)
-  exte.mem.ar.bits.size := "b010".U
-  
-  assert(!exte.mem.aw.valid && !exte.mem.w.valid && !exte.mem.b.valid)
-
-  exte.mem.ar.valid := state === sIdle && canValid
-
   val nextState = MuxLookup(state, sIdle)(
     Seq(
-      sIdle -> Mux(exte.mem.ar.ready && canValid, sWaitResp, sIdle),
-      sWaitResp -> Mux(exte.mem.r.valid, Mux(out.fire, sIdle, sKeepData), sWaitResp),
+      sIdle -> Mux(
+        exte.mem.ar.fire,
+        Mux(exte.mem.r.valid, Mux(out.fire, sIdle, sKeepData), sWaitResp),
+        sIdle
+      ),
+      sWaitResp -> Mux(
+        exte.mem.r.valid,
+        Mux(out.fire, sIdle, sKeepData),
+        sWaitResp
+      ),
       sKeepData -> Mux(out.fire, sIdle, sKeepData)
-
     )
   )
   state := nextState
 
-  // out.valid := (state === sWaitResp && exte.mem.r.valid) || state === sKeepData
-  out.valid := state === sKeepData
-  val dataReg = RegEnable(exte.mem.r.bits.data, state === sWaitResp && exte.mem.r.valid && !out.fire)
-  // outBits.ifuPayload.ifu.inst := Mux(state === sKeepData, dataReg, exte.mem.r.bits.data)
-  outBits.ifuPayload.ifu.inst := dataReg
-  
-  val pc = exte.pcReg.pc
+  exte.mem :<= 0.U.asTypeOf(new Axi4IO)
+  exte.mem.ar.bits.size := "b010".U
+  assert(!exte.mem.aw.valid && !exte.mem.w.valid && !exte.mem.b.valid)
+  exte.mem.ar.valid := state === sIdle && canValid
   exte.mem.ar.bits.addr := pc
-  outBits.ifuPayload.ifu.pc := pc
+  exte.mem.r.ready := true.B
 
-  exte.mem.r.ready := state === sWaitResp
+  val dataReg = RegEnable(
+    exte.mem.r.bits.data,
+    state =/= sKeepData && nextState === sKeepData
+  )
+  // out.valid := (state === sWaitResp && exte.mem.r.valid) || state === sKeepData
+  // out.valid := state === sKeepData
+  out.valid := exte.mem.r.valid || state === sKeepData
+  // outBits.ifuPayload.ifu.inst := dataReg
+  outBits.ifuPayload.ifu.inst := Mux(state === sKeepData, dataReg, exte.mem.r.bits.data)
+  outBits.ifuPayload.ifu.pc := pc
 
   if (cfg.isDebug) {
     debug.get := outBits.ifuPayload.ifu.inst
   }
-  PerfWhen("instFetch", exte.mem.r.fire, out.valid && (out.bits.ifuPayload.ifu.inst === "h00100073".U))
-  PerfWhen("waitReadCyc", state === sIdle || state === sWaitResp, out.valid && (out.bits.ifuPayload.ifu.inst === "h00100073".U))
-  PerfWhen("keepDataCyc", state === sKeepData, out.valid && (out.bits.ifuPayload.ifu.inst === "h00100073".U))
+  PerfWhen(
+    "instFetch",
+    exte.mem.r.fire,
+    out.valid && (out.bits.ifuPayload.ifu.inst === "h00100073".U)
+  )
+  PerfWhen(
+    "waitReadCyc",
+    state === sIdle || state === sWaitResp,
+    out.valid && (out.bits.ifuPayload.ifu.inst === "h00100073".U)
+  )
+  PerfWhen(
+    "keepDataCyc",
+    state === sKeepData,
+    out.valid && (out.bits.ifuPayload.ifu.inst === "h00100073".U)
+  )
 }
