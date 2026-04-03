117207eab604f5b6929e4708dc83ea308d44b189
tracer-ysyx <tracer@ysyx.org>
2026-04-03 10:28:30 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:28:30 up 46 min,  1 user,  load average: 0.01, 0.03, 0.00

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -32,7 +32,7 @@ static inline void serial_io_handler(uint32_t offset, int len, bool is_write) {
   if (!is_write) {
     return;
   }
-  assert(len == 1);
+  // assert(len == 1);
   switch (offset) {
     /* We bind the serial port with the host stderr in NEMU. */
     case CH_OFFSET:
