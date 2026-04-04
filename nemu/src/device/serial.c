007601c534598d98b17026ff830a426ae03aa12e
tracer-ysyx <tracer@ysyx.org>
2026-04-04 11:46:06 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:46:06 up 42 min,  1 user,  load average: 0.14, 0.17, 0.11

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -39,13 +39,13 @@ static inline void serial_io_handler(uint32_t offset, int len, bool is_write) {
       if (is_write) serial_putc(serial_base[0]);
       else panic("do not support read");
       break;
-    default: panic("do not support offset = %d", offset);
+    IFNDEF(CONFIG_SIM_SOC, default: panic("do not support offset = %d", offset));
   }
 }
 
 void init_serial() {
   serial_base = new_space(8);
-  // serial_base[5] = 0x20;
+  IFDEF(CONFIG_SIM_SOC, serial_base[5] = 0x20);
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("serial", CONFIG_SERIAL_PORT, serial_base, 8, serial_io_handler);
 #else
