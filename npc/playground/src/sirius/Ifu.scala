18764495b2f334a86bfad349534a3d396c17b4ad
tracer-ysyx <tracer@ysyx.org>
2026-04-05 15:43:44 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:43:44 up  1:35,  1 user,  load average: 1.27, 0.69, 0.67

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -88,6 +88,7 @@ class Icache(
 
   when(io.mem.r.fire && inWhiteList) {
     line.data(cacheWPtr) := io.mem.r.bits.data
+    printf("%x\n", io.mem.r.bits.data)
     line.tag := rAddrLine.tag
     lineValid := true.B
   }
