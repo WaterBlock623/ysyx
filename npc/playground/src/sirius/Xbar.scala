b10278ee2c78a081df916808931a3cdc7cf727a8
tracer-ysyx <tracer@ysyx.org>
2026-04-06 18:38:57 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:38:57 up  6:00,  1 user,  load average: 0.81, 0.74, 0.45

--- npc/playground/src/sirius/Xbar.scala
+++ npc/playground/src/sirius/Xbar.scala
@@ -6,11 +6,11 @@ import chisel3.SpecifiedDirection.Flip
 
 class Xbar(
   nMasters: Int = 2,
-  nSlaves: Int = 2,
-  routeFn: Seq[UInt => Bool],
+  nSlaves:  Int = 2,
+  routeFn:  Seq[UInt => Bool],
   busWidth: Int = 32,
-  idWidth: Int = 4
-) extends Module {
+  idWidth:  Int = 4)
+    extends Module {
 
   require(routeFn.length == nSlaves)
 
@@ -20,13 +20,13 @@ class Xbar(
   val lowIdBits = idWidth - masterIdBits
 
   val io = IO(new Bundle {
-    val in  = Flipped(Vec(nMasters, new Axi4IO(busWidth)))
+    val in = Flipped(Vec(nMasters, new Axi4IO(busWidth)))
     val out = Vec(nSlaves, new Axi4IO(busWidth))
   })
 
   for (m <- 0 until nMasters) {
-    assert(io.in(m).aw.bits.id(idWidth-1, lowIdBits) === 0.U)
-    assert(io.in(m).ar.bits.id(idWidth-1, lowIdBits) === 0.U)
+    assert(io.in(m).aw.bits.id(idWidth - 1, lowIdBits) === 0.U)
+    assert(io.in(m).ar.bits.id(idWidth - 1, lowIdBits) === 0.U)
   }
 
   def decode(addr: UInt): Vec[Bool] = {
@@ -37,10 +37,12 @@ class Xbar(
   0.U.asTypeOf(chiselTypeOf(io.in)) :>= io.in
 
   // AW W
+  val awReady = Wire(Vec(nMasters, Vec(nSlaves, Bool())))
+  val wReady = Wire(Vec(nMasters, Vec(nSlaves, Bool())))
   for (s <- 0 until nSlaves) {
 
     val awArb = Module(new Arbiter(chiselTypeOf(io.in(0).aw.bits), nMasters))
-    val wArb  = Module(new Arbiter(chiselTypeOf(io.in(0).w.bits), nMasters))
+    val wArb = Module(new Arbiter(chiselTypeOf(io.in(0).w.bits), nMasters))
 
     for (m <- 0 until nMasters) {
       val sel = decode(io.in(m).aw.bits.addr)(s)
@@ -48,57 +50,61 @@ class Xbar(
       val fireCond = io.in(m).aw.valid && io.in(m).w.valid && sel
 
       awArb.io.in(m).valid := fireCond
-      awArb.io.in(m).bits  := io.in(m).aw.bits
+      awArb.io.in(m).bits := io.in(m).aw.bits
       awArb.io.in(m).bits.id :=
         Cat(m.U(masterIdBits.W), io.in(m).aw.bits.id(lowIdBits - 1, 0))
 
       wArb.io.in(m).valid := fireCond
-      wArb.io.in(m).bits  := io.in(m).w.bits
+      wArb.io.in(m).bits := io.in(m).w.bits
 
-      io.in(m).aw.ready := awArb.io.in(m).ready && fireCond
-      io.in(m).w.ready  := wArb.io.in(m).ready && fireCond
+      // io.in(m).aw.ready := awArb.io.in(m).ready && fireCond
+      // io.in(m).w.ready := wArb.io.in(m).ready && fireCond
+      awReady(m)(s) := awArb.io.in(m).ready && fireCond
+      wReady(m)(s) := wArb.io.in(m).ready && fireCond
     }
 
     io.out(s).aw <> awArb.io.out
-    io.out(s).w  <> wArb.io.out
+    io.out(s).w <> wArb.io.out
+  }
+  for (m <- 0 until nMasters) {
+    io.in(m).aw.ready := awReady(m).reduce(_ || _)
+    io.in(m).w.ready := wReady(m).reduce(_ || _)
   }
 
   // AR
+  val arReady = Wire(Vec(nMasters, Vec(nSlaves, Bool())))
   for (s <- 0 until nSlaves) {
-
     val arArb = Module(new Arbiter(chiselTypeOf(io.in(0).ar.bits), nMasters))
 
     for (m <- 0 until nMasters) {
       val sel = decode(io.in(m).ar.bits.addr)(s)
-      val debugArSel = dontTouch(WireDefault(sel))
 
       arArb.io.in(m).valid := io.in(m).ar.valid && sel
-      arArb.io.in(m).bits  := io.in(m).ar.bits
+      arArb.io.in(m).bits := io.in(m).ar.bits
       arArb.io.in(m).bits.id :=
         Cat(m.U(masterIdBits.W), io.in(m).ar.bits.id(lowIdBits - 1, 0))
 
-      val debugArReady = dontTouch(arArb.io.in(m).ready && sel)
-      // io.in(m).ar.ready := arArb.io.in(m).ready && sel
-      io.in(m).ar.ready := debugArReady
-      when (arArb.io.in(m).ready) {
-        printf(s"Master$m Slave$s ready:%d sel:%d", arArb.io.in(m).ready, sel)
-      }
+      arReady(m)(s) := arArb.io.in(m).ready && sel
     }
 
     io.out(s).ar <> arArb.io.out
   }
+  for (m <- 0 until nMasters) {
+    io.in(m).ar.ready := arReady(m).reduce(_ || _)
+  }
+  
 
   // B
   for (s <- 0 until nSlaves) {
     val bid = io.out(s).b.bits.id
 
     val masterSel = bid(idWidth - 1, lowIdBits)
-    val realId    = bid(lowIdBits - 1, 0)
+    val realId = bid(lowIdBits - 1, 0)
 
     for (m <- 0 until nMasters) {
-      when (masterSel === m.U) {
+      when(masterSel === m.U) {
         io.in(m).b.valid := io.out(s).b.valid
-        io.in(m).b.bits  := io.out(s).b.bits
+        io.in(m).b.bits := io.out(s).b.bits
         io.in(m).b.bits.id := realId
 
         io.out(s).b.ready := io.in(m).b.ready
@@ -111,12 +117,12 @@ class Xbar(
     val rid = io.out(s).r.bits.id
 
     val masterSel = rid(idWidth - 1, lowIdBits)
-    val realId    = rid(lowIdBits - 1, 0)
+    val realId = rid(lowIdBits - 1, 0)
 
     for (m <- 0 until nMasters) {
-      when (masterSel === m.U) {
+      when(masterSel === m.U) {
         io.in(m).r.valid := io.out(s).r.valid
-        io.in(m).r.bits  := io.out(s).r.bits
+        io.in(m).r.bits := io.out(s).r.bits
         io.in(m).r.bits.id := realId
 
         io.out(s).r.ready := io.in(m).r.ready
@@ -270,7 +276,7 @@ class ClintDevice extends Module {
   }
   in.r.bits.last := true.B
   val idReg = Reg(chiselTypeOf(in.ar.bits.id))
-  when (in.ar.fire) {
+  when(in.ar.fire) {
     idReg := in.ar.bits.id
   }
   in.r.bits.id := idReg
