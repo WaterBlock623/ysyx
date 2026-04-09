b04fe891f890486ca638a5b5a75380ea31d09411
tracer-ysyx <tracer@ysyx.org>
2026-04-09 12:21:26 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:21:26 up  1:10,  1 user,  load average: 6.82, 4.75, 2.75

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -151,8 +151,8 @@ class Ifu(
 
   val icache = Module(
     new Icache(
-      lineNum = 16,
-      lineByte = 8,
+      lineNum = 4,
+      lineByte = 32,
       busByte = 4,
       if (cfg.ysyxsoc) {
         Some(BigInt("a0000000", 16) until BigInt("c0000000", 16))
