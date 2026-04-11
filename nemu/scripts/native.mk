dec426e0b01a9ae6157161a65fe01112da8d83c7
tracer-ysyx <tracer@ysyx.org>
2026-04-11 21:07:02 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:07:02 up 10:53,  1 user,  load average: 0.02, 0.32, 0.57

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -35,7 +35,8 @@ IMG ?=
 # NEMU_EXEC := numactl -m 0 -C 0,2,4,6 -- $(BINARY) $(ARGS) $(IMG)
 _NEMU_EXEC = set -o pipefail; \
 						 stdbuf -oL $(BINARY) $(ARGS) $(IMG) 2>&1 | \
-						 tee $(BUILD_DIR)/std-output$(if $(TASK_NAME),-$(TASK_NAME),).txt
+						 tee $(BUILD_DIR)/std-output$(if $(TASK_NAME),-$(TASK_NAME),).txt; \
+						 sleep 5
 ifeq ($(CONFIG_DEBUGER_GDB),y)
 $(info GDB_SOCKET $(GDB_SOCKET))
 ifneq ($(GDB_ELF),)
