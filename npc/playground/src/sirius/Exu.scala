c43e6091e7de90ea3b7f7714ea422036efdaaa04
tracer-ysyx <tracer@ysyx.org>
2026-04-11 21:20:59 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:20:59 up 11:08,  1 user,  load average: 1.66, 1.68, 1.42

--- npc/playground/src/sirius/Exu.scala
+++ npc/playground/src/sirius/Exu.scala
@@ -28,44 +28,33 @@ class AluBase(
     extends AluParent {
   import AluOpEnum._
 
-  // val isSub = io.aluOp === sub.asUInt
-  // val negSrc2 = ~io.src2 + 1.U
-  // val addSubResult = io.src1 + Mux(isSub, negSrc2, io.src2)
-  //
-  // io.out := RegNext(MuxLookup(io.aluOp, addSubResult)(
-  //   Seq(
-  //     add.asUInt -> addSubResult,
-  //     sub.asUInt -> addSubResult,
-  //   )
-  // ))
-
-  val useSub = Seq(sub, eql, neq, lt, ltu, ge, geu, clear)
-    .map(io.aluOp === _.asUInt)
-    .reduce(_ || _)
-  val xorSrc2 = io.src2 ^ Fill(io.src2.getWidth, useSub)
-  val addSub = io.src1 +& xorSrc2 + useSub.asUInt
-  val addSubResult = addSub.tail(1)
-  val addResult = addSubResult
-  val subResult = addSubResult
-  val eqlResult = addSubResult === 0.U
-  val overflow = io.src1.head(1) === xorSrc2.head(1) && io.src1.head(1) =/= addSubResult.head(1)
-  val ltResult = addSubResult.head(1) ^ overflow
-  val geResult = !ltResult
-  val geuResult = addSub.head(1)
-  val ltuResult = !geuResult
-  val andClearResult = io.src1 & xorSrc2
-  val andResult = andClearResult
-  val clearResult = andClearResult
-
-  // val addResult = io.src1 + io.src2
-  // val subResult = io.src1 - io.src2
-  // val eqlResult = io.src1 === io.src2
-  val neqResult = !eqlResult
-  // val ltResult = io.src1.asSInt < io.src2.asSInt
-  // val ltuResult = io.src1 < io.src2
+  // val useSub = Seq(sub, eql, neq, lt, ltu, ge, geu, clear)
+  //   .map(io.aluOp === _.asUInt)
+  //   .reduce(_ || _)
+  // val xorSrc2 = io.src2 ^ Fill(io.src2.getWidth, useSub)
+  // val addSub = io.src1 +& xorSrc2 + useSub.asUInt
+  // val addSubResult = addSub.tail(1)
+  // val addResult = addSubResult
+  // val subResult = addSubResult
+  // val eqlResult = addSubResult === 0.U
+  // val overflow = io.src1.head(1) === xorSrc2.head(1) && io.src1.head(1) =/= addSubResult.head(1)
+  // val ltResult = addSubResult.head(1) ^ overflow
   // val geResult = !ltResult
-  // val geuResult = !ltuResult
-  // val andResult = io.src1 & io.src2
+  // val geuResult = addSub.head(1)
+  // val ltuResult = !geuResult
+  // val andClearResult = io.src1 & xorSrc2
+  // val andResult = andClearResult
+  // val clearResult = andClearResult
+
+  val addResult = io.src1 + io.src2
+  val subResult = io.src1 - io.src2
+  val eqlResult = io.src1 === io.src2
+  val neqResult = !eqlResult
+  val ltResult = io.src1.asSInt < io.src2.asSInt
+  val ltuResult = io.src1 < io.src2
+  val geResult = !ltResult
+  val geuResult = !ltuResult
+  val andResult = io.src1 & io.src2
   val orResult = io.src1 | io.src2
   val xorResult = io.src1 ^ io.src2
   val shiftNum = io.src2(log2Ceil(cfg.xlen) - 1, 0)
@@ -73,26 +62,36 @@ class AluBase(
   val srlResult = io.src1 >> shiftNum
   val sraResult = (io.src1.asSInt >> shiftNum).asUInt
   val direct1Result = io.src1
-  // val clearResult = io.src1 & ~io.src2
+  val clearResult = io.src1 & ~io.src2
 
   io.out := addResult
   switch(io.aluOp) {
-    is(add.asUInt) {io.out := addResult; assert(io.out === (io.src1 + io.src2))}
-    is(sub.asUInt) {io.out := subResult; assert(io.out === (io.src1 - io.src2))}
-    is(eql.asUInt) {io.out := eqlResult; assert(io.out === (io.src1 === io.src2))}
-    is(neq.asUInt) {io.out := neqResult; assert(io.out === (io.src1 =/= io.src2))}
-    is(lt.asUInt) {io.out := ltResult; assert(io.out === (io.src1.asSInt < io.src2.asSInt))}
-    is(ltu.asUInt) {io.out := ltuResult; assert(io.out === (io.src1 < io.src2))}
-    is(ge.asUInt) {io.out := geResult; assert(io.out === (io.src1.asSInt >= io.src2.asSInt))}
-    is(geu.asUInt) {io.out := geuResult; assert(io.out === (io.src1 >= io.src2))}
-    is(and.asUInt) {io.out := andResult; assert(io.out === (io.src1 & io.src2))}
+    // is(add.asUInt) {io.out := addResult; assert(io.out === (io.src1 + io.src2))}
+    // is(sub.asUInt) {io.out := subResult; assert(io.out === (io.src1 - io.src2))}
+    // is(eql.asUInt) {io.out := eqlResult; assert(io.out === (io.src1 === io.src2))}
+    // is(neq.asUInt) {io.out := neqResult; assert(io.out === (io.src1 =/= io.src2))}
+    // is(lt.asUInt) {io.out := ltResult; assert(io.out === (io.src1.asSInt < io.src2.asSInt))}
+    // is(ltu.asUInt) {io.out := ltuResult; assert(io.out === (io.src1 < io.src2))}
+    // is(ge.asUInt) {io.out := geResult; assert(io.out === (io.src1.asSInt >= io.src2.asSInt))}
+    // is(geu.asUInt) {io.out := geuResult; assert(io.out === (io.src1 >= io.src2))}
+    // is(and.asUInt) {io.out := andResult; assert(io.out === (io.src1 & io.src2))}
+    is(add.asUInt) {io.out := addResult}
+    is(sub.asUInt) {io.out := subResult}
+    is(eql.asUInt) {io.out := eqlResult}
+    is(neq.asUInt) {io.out := neqResult}
+    is(lt.asUInt) {io.out := ltResult}
+    is(ltu.asUInt) {io.out := ltuResult}
+    is(ge.asUInt) {io.out := geResult}
+    is(geu.asUInt) {io.out := geuResult}
+    is(and.asUInt) {io.out := andResult}
     is(or.asUInt) {io.out := orResult}
     is(xor.asUInt) {io.out := xorResult}
     is(sll.asUInt) {io.out := sllResult}
     is(srl.asUInt) {io.out := srlResult}
     is(sra.asUInt) {io.out := sraResult}
     is(direct1.asUInt) {io.out := direct1Result}
-    is(clear.asUInt) {io.out := clearResult; assert(io.out === (io.src1 & ~io.src2))}
+    // is(clear.asUInt) {io.out := clearResult; assert(io.out === (io.src1 & ~io.src2))}
+    is(clear.asUInt) {io.out := clearResult}
   }
   // io.out := MuxLookup(io.aluOp, addResult)(
   //   Seq(
