d8d2935622b91c97194811a0ff7eaa9e13881f94
tracer-ysyx <tracer@ysyx.org>
2026-04-11 16:08:16 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:08:16 up  5:42,  1 user,  load average: 0.34, 0.21, 0.35

--- npc/playground/src/sirius/RegisterFile.scala
+++ npc/playground/src/sirius/RegisterFile.scala
@@ -8,29 +8,29 @@ class RegisterFile(implicit private val cfg: CoreConfig) extends Module {
   val debug = Option.when(cfg.isDebug)(IO(Output(Vec(cfg.registerNum, UInt(cfg.xlen.W)))))
   // val debug = IO(Output(Vec(cfg.registerNum, UInt(cfg.xlen.W))))
 
-  // val regFile = Reg(Vec(cfg.registerNum, UInt(cfg.xlen.W)))
-  // when(wbuIn.wEn) {
-  //   regFile(wbuIn.wAddr) := wbuIn.wData
-  // }
-  // regFile(0) := 0.U
-  // iduIn.rData(0) := regFile(iduIn.rAddr(0))
-  // iduIn.rData(1) := regFile(iduIn.rAddr(1))
-  //
-  // if (cfg.isDebug) {
-  //   debug.get := regFile
-  // }
+  val regFile = Reg(Vec(cfg.registerNum, UInt(cfg.xlen.W)))
+  when(wbuIn.wEn) {
+    regFile(wbuIn.wAddr) := wbuIn.wData
+  }
+  regFile(0) := 0.U
+  iduIn.rData(0) := regFile(iduIn.rAddr(0))
+  iduIn.rData(1) := regFile(iduIn.rAddr(1))
+
+  if (cfg.isDebug) {
+    debug.get := regFile
+  }
   // debug := regFile
 
-  val regFile = Mem(cfg.registerNum, UInt(cfg.xlen.W))
-  when(wbuIn.wEn && (wbuIn.wAddr =/= 0.U)) {
-    regFile.write(wbuIn.wAddr, wbuIn.wData)
-  }
-  iduIn.rData(0) := regFile.read(iduIn.rAddr(0))
-  iduIn.rData(1) := regFile.read(iduIn.rAddr(1))
-  when (iduIn.rAddr(0) === 0.U) {
-    iduIn.rData(0) := 0.U
-  }
-  when (iduIn.rAddr(1) === 0.U) {
-    iduIn.rData(1) := 0.U
-  }
+  // val regFile = Mem(cfg.registerNum, UInt(cfg.xlen.W))
+  // when(wbuIn.wEn && (wbuIn.wAddr =/= 0.U)) {
+  //   regFile.write(wbuIn.wAddr, wbuIn.wData)
+  // }
+  // iduIn.rData(0) := regFile.read(iduIn.rAddr(0))
+  // iduIn.rData(1) := regFile.read(iduIn.rAddr(1))
+  // when (iduIn.rAddr(0) === 0.U) {
+  //   iduIn.rData(0) := 0.U
+  // }
+  // when (iduIn.rAddr(1) === 0.U) {
+  //   iduIn.rData(1) := 0.U
+  // }
 }
