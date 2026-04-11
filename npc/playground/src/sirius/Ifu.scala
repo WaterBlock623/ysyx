5bf4c0eb6d83007b6a81fe3da6f24534348f5593
tracer-ysyx <tracer@ysyx.org>
2026-04-11 19:15:54 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:15:54 up  8:57,  1 user,  load average: 1.17, 0.77, 0.84

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -152,51 +152,53 @@ class Icache(
   val wayIdx = Mux(isAllValid, allValidWayIdx, invalidWayIdx)
   cache.io.wayIdx := wayIdx
 
-  // val (isHit, hitData) = valids(setIdx).zip(cache.io.rData).map { case (valid, line) =>
-  //   val isHit = valid && (line.tag === rAddrLine.tag)
-  //   val data = line.data.asUInt & Fill(line.data.getWidth, isHit)
-  //   (isHit, data.asTypeOf(chiselTypeOf(cache.io.rData.head.data)))
-  // }.reduce { (a, b) => 
-  //   (a._1 || b._1, (a._2.asUInt | b._2.asUInt).asTypeOf(chiselTypeOf(cache.io.rData.head.data))) 
-  // }
+  val (isHit, hitData) = valids(setIdx).zip(cache.io.rData).map { case (valid, line) =>
+    val isHit = valid && (line.tag === rAddrLine.tag)
+    val data = line.data.asUInt & Fill(line.data.getWidth, isHit)
+    (isHit, data.asTypeOf(chiselTypeOf(cache.io.rData.head.data)))
+  }.reduce { (a, b) => 
+    (a._1 || b._1, (a._2.asUInt | b._2.asUInt).asTypeOf(chiselTypeOf(cache.io.rData.head.data))) 
+  }
 
-  val hits = VecInit(valids(setIdx).zip(cache.io.rData).map { case (valid, line) =>
-    valid && (line.tag === rAddrLine.tag)
-  })
-  val isHit = hits.asUInt =/= 0.U
-  val hitData = cache.io.rData(OHToUInt(hits)).data
+  // val hits = VecInit(valids(setIdx).zip(cache.io.rData).map { case (valid, line) =>
+  //   valid && (line.tag === rAddrLine.tag)
+  // })
+  // val isHit = hits.asUInt =/= 0.U
+  // val hitData = cache.io.rData(OHToUInt(hits)).data
 
   // FSM
   val sIdle :: sReadCache :: sReq :: sFirstResp :: sFillCache :: Nil = Enum(5)
   val state = RegInit(sIdle)
+  val nextState = WireDefault(state)
+  state := nextState
   switch(state) {
     is(sIdle) {
       when (io.cached.ar.valid) {
-        state := sReadCache
+        nextState := sReadCache
       }
     }
     is(sReadCache) {
       when(isHit && inWhiteList) {
         when(io.cached.r.fire) {
-          state := sIdle
+          nextState := sIdle
         }
       } .otherwise {
-        state := sReq
+        nextState := sReq
       }
     }
     is(sReq) {
       when(io.mem.ar.fire) {
-        state := sFirstResp
+        nextState := sFirstResp
       }
     }
     is(sFirstResp) {
       when (io.mem.r.fire) {
-        state := Mux(io.mem.r.bits.last, sIdle, sFillCache)
+        nextState := Mux(io.mem.r.bits.last, sIdle, sFillCache)
       }
     }
     is(sFillCache) {
       when (io.mem.r.fire && io.mem.r.bits.last) {
-        state := sIdle
+        nextState := sIdle
       }
     }
   }
