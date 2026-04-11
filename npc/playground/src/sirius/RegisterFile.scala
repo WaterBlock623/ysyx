33b6e7a47217ba2919898700dc1dabf1e24019a3
tracer-ysyx <tracer@ysyx.org>
2026-04-11 15:26:45 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:26:45 up  4:59,  1 user,  load average: 0.19, 0.29, 0.65

--- npc/playground/src/sirius/RegisterFile.scala
+++ npc/playground/src/sirius/RegisterFile.scala
@@ -8,16 +8,24 @@ class RegisterFile(implicit private val cfg: CoreConfig) extends Module {
   val debug = Option.when(cfg.isDebug)(IO(Output(Vec(cfg.registerNum, UInt(cfg.xlen.W)))))
   // val debug = IO(Output(Vec(cfg.registerNum, UInt(cfg.xlen.W))))
 
-  val regFile = Reg(Vec(cfg.registerNum, UInt(cfg.xlen.W)))
+  // val regFile = Reg(Vec(cfg.registerNum, UInt(cfg.xlen.W)))
+  // when(wbuIn.wEn) {
+  //   regFile(wbuIn.wAddr) := wbuIn.wData
+  // }
+  // regFile(0) := 0.U
+  // iduIn.rData(0) := regFile(iduIn.rAddr(0))
+  // iduIn.rData(1) := regFile(iduIn.rAddr(1))
+  //
+  // if (cfg.isDebug) {
+  //   debug.get := regFile
+  // }
+  // debug := regFile
+
+  val regFile = Mem(cfg.registerNum, UInt(cfg.xlen.W))
   when(wbuIn.wEn) {
-    regFile(wbuIn.wAddr) := wbuIn.wData
+    regFile.write(wbuIn.wAddr, wbuIn.wData)
   }
   regFile(0) := 0.U
-  iduIn.rData(0) := regFile(iduIn.rAddr(0))
-  iduIn.rData(1) := regFile(iduIn.rAddr(1))
-
-  if (cfg.isDebug) {
-    debug.get := regFile
-  }
-  // debug := regFile
+  iduIn.rData(0) := regFile.read(iduIn.rAddr(0))
+  iduIn.rData(1) := regFile.read(iduIn.rAddr(1))
 }
