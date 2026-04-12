55e7945d650f46d506c8dd173ecadd7eaa24c384
tracer-ysyx <tracer@ysyx.org>
2026-04-12 16:41:52 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:41:52 up  3:30,  1 user,  load average: 0.20, 0.34, 0.39

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -31,11 +31,13 @@ inline paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBAS
 
 static inline word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
+  IFDEF(CONFIG_TARGET_SHARE, printf("SHARE"));
   printf("[NEMU] READ %d Byte: *" FMT_PADDR "=" FMT_WORD "\n", len, addr, ret);
   return ret;
 }
 
 static inline void pmem_write(paddr_t addr, int len, word_t data) {
+  IFDEF(CONFIG_TARGET_SHARE, printf("SHARE"));
   printf("[NEMU] WRITE %d Byte: *" FMT_PADDR "=" FMT_WORD "\n", len, addr, data);
   host_write(guest_to_host(addr), len, data);
 }
