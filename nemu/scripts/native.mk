913cf8127ee993ca6319e9a3cce3985c1495a66b
tracer-ysyx <tracer@ysyx.org>
2026-04-12 18:19:40 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:19:40 up  5:15,  1 user,  load average: 0.14, 0.45, 0.36

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -33,8 +33,9 @@ $(info NEMU BUILD_DIR $(BUILD_DIR))
 # Command to execute NEMU
 IMG ?=
 # NEMU_EXEC := numactl -m 0 -C 0,2,4,6 -- $(BINARY) $(ARGS) $(IMG)
+_NEMU_EXEC_RAW = $(BINARY) $(ARGS) $(IMG)
 _NEMU_EXEC = set -o pipefail; \
-						 (stdbuf -oL $(BINARY) $(ARGS) $(IMG) 2>&1 | \
+						 (stdbuf -oL $(_NEMU_EXEC_RAW) 2>&1 | \
 						 tee $(BUILD_DIR)/std-output$(if $(TASK_NAME),-$(TASK_NAME),).txt; \
 						 exit $$?)
 ifeq ($(CONFIG_DEBUGER_GDB),y)
@@ -73,7 +74,7 @@ gdb: SHELL := /bin/bash
 gdb: run-env
 	-@mkdir -p $(BUILD_DIR)/profile/
 	$(call git_commit, "gdb NEMU")
-	gdb -s $(BINARY) --args $(_NEMU_EXEC)
+	gdb -s $(BINARY) --args $(_NEMU_EXEC_RAW)
 	-@mv -f $(NEMU_HOME)/profile.vlt $(BUILD_DIR)/profile/profile.vlt >/dev/null 2>&1
 	-@mv -f $(NEMU_HOME)/profile_exec.dat $(BUILD_DIR)/profile/profile_exec.dat >/dev/null 2>&1
 
