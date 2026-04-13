dc24c6b628af03a53431af719d52427855c74c22
tracer-ysyx <tracer@ysyx.org>
2026-04-13 17:36:04 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:36:04 up  3:53,  1 user,  load average: 1.88, 0.95, 0.40

--- npc/playground/src/sirius/Bundles.scala
+++ npc/playground/src/sirius/Bundles.scala
@@ -57,9 +57,9 @@ class ExuCtrl(implicit private val cfg: CoreConfig) extends LsuCtrl {
   val exuCtrl = new CtrlSignals().ex
 }
 
-class IfuCtrl(implicit private val cfg: CoreConfig) extends Bundle {
-  val ifuCtrl = new CtrlSignals().if
-}
+// class IfuCtrl(implicit private val cfg: CoreConfig) extends Bundle {
+//   val ifuCtrl = new CtrlSignals().if
+// }
 
 // IO
 class IfuToIduIO(implicit private val cfg: CoreConfig) extends Bundle {
