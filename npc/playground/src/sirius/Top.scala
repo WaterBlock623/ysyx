ff9a8a1b3a6ec1bc7a5226ac30a749bf3e818cae
tracer-ysyx <tracer@ysyx.org>
2026-04-16 15:32:47 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:32:47 up  1:57,  1 user,  load average: 1.05, 0.41, 0.45

--- npc/playground/src/sirius/Top.scala
+++ npc/playground/src/sirius/Top.scala
@@ -38,17 +38,6 @@ class Top(
 
   val globalCtrl = idu.exte.globalCtrl
 
-  if (cfg.perf && cfg.isDebug) {
-    val ebreaks = Seq(
-      ifu.exte.debugEbreak,
-      idu.exte.debugEbreak,
-      exu.exte.debugEbreak,
-      lsu.exte.debugEbreak,
-      wbu.exte.debugEbreak
-    )
-    ebreaks.foreach {_.get := wbu.in.bits.ctrl.debugCtrl.get.isEbreak}
-  }
-
   if (cfg.ysyxsoc) {
     val io = IO(new Bundle {
       val interrupt = Input(Bool())
@@ -67,6 +56,15 @@ class Top(
   }
 
   if (cfg.isDebug) {
+    val ebreaks = Seq(
+      ifu.exte.debugEbreak,
+      idu.exte.debugEbreak,
+      exu.exte.debugEbreak,
+      lsu.exte.debugEbreak,
+      wbu.exte.debugEbreak
+    )
+    ebreaks.foreach {_.get := wbu.in.bits.ctrl.debugCtrl.get.isEbreak}
+
     val debugInfoDpiC = Module(new DebugInfoDpiC)
     val getGprDpiC = Module(new GetGprDpiC)
     debugInfoDpiC.isEbreak := wbu.in.bits.ctrl.debugCtrl.get.isEbreak
