c0774ff9bd0623e1ba01dbd64f64f200455f252a
tracer-ysyx <tracer@ysyx.org>
2026-04-19 13:57:36 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:57:36 up 27 min,  1 user,  load average: 0.52, 0.69, 0.43

--- npc/csrc/riscv32_npc/reg.c
+++ npc/csrc/riscv32_npc/reg.c
@@ -43,7 +43,7 @@ bool isa_try_find_reg(int regno, void **reg, size_t *len) {
   }
   switch (regno) {
     case 32:
-    case LENGTH(cpu.gpr):
+    // case LENGTH(cpu.gpr):
       safe_deref(reg, &cpu.pc);
       safe_deref(len, MUXDEF(CONFIG_ISA64, 8, 4));
       return true;
