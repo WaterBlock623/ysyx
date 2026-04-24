9b9d266a9091015eccdf9212acaedf0d60e7c133
tracer-ysyx <tracer@ysyx.org>
2026-04-24 14:35:26 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:35:26 up  2:30,  1 user,  load average: 0.26, 0.60, 0.49

--- npc/playground/src/sirius/Top.scala
+++ npc/playground/src/sirius/Top.scala
@@ -183,6 +183,9 @@ class BasicCore(
 
       perfPipeline("ifu", !reset.asBool, idu.in.valid, idu.in.ready)
       perfPipeline("idu", idu.in.valid, exu.in.valid, exu.in.ready, Map("RawGpr" -> isRawGpr))
+      when (RegNext(idu.in.valid) && !exu.in.valid && !isRawGpr) {
+        assert(0)
+      }
       perfPipeline(
         "exu",
         exu.in.valid,
