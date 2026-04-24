6f639ab8e293861792bee807ecd9479013633bf7
tracer-ysyx <tracer@ysyx.org>
2026-04-24 14:19:04 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:19:04 up  2:15,  1 user,  load average: 0.70, 0.42, 0.43

--- npc/playground/src/sirius/Top.scala
+++ npc/playground/src/sirius/Top.scala
@@ -173,7 +173,11 @@ class BasicCore(
         val isStageStall = RegNext(thisInValid) && !nextInValid
         PerfWhen(s"${name}TotalStallCyc", isStageStall, Some(stopFlag))
         cause.foreach { case (condName, cond) =>
-          PerfWhen(s"${name}${condName}StallCyc", isStageStall && RegNext(cond), Some(stopFlag))
+          PerfWhen(
+            s"${name}${condName}StallCyc",
+            isStageStall && (cond || RegNext(cond)),
+            Some(stopFlag)
+          )
         }
       }
 
