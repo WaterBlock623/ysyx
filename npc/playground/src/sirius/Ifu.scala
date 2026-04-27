6fdd82a19a696e0ea785c5fbbbfdb69ee6933f3d
tracer-ysyx <tracer@ysyx.org>
2026-04-27 19:58:36 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:58:36 up 11:05,  1 user,  load average: 0.04, 0.33, 0.57

--- npc/playground/src/sirius/Ifu.scala
+++ npc/playground/src/sirius/Ifu.scala
@@ -299,7 +299,7 @@ class Icache(
 
   0.U.asTypeOf(chiselTypeOf(io.cached)) :>= io.cached
   // io.cached.ar.ready := state === sIdle && (!cachedRValidReg || io.cached.r.fire)
-  io.cached.ar.ready := nextState === sReadCache
+  io.cached.ar.ready := !abortReg && nextState === sReadCache
   io.cached.r.valid := !abortReg && ((io.cached.ar.valid && state === sReadCache && isHit) || cachedRValidReg)
   io.cached.r.bits.data := Mux(
     cachedRValidReg,
