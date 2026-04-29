0c6549a0ddb8a8e1e164e51c010a9ee4246ae3fb
tracer-ysyx <tracer@ysyx.org>
2026-04-29 18:40:22 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:40:22 up  8:03,  1 user,  load average: 0.56, 0.60, 0.45

--- npc/playground/src/sirius/RegisterFile.scala
+++ npc/playground/src/sirius/RegisterFile.scala
@@ -8,38 +8,61 @@ class RegisterFile(
   val iduIn = IO(Flipped(new IduToRegFileIO))
   val wbuIn = IO(Flipped(new WbuToRegFileIO))
   val debug = Option.when(cfg.isDebug)(IO(Output(Vec(cfg.registerNum, UInt(cfg.xlen.W)))))
-  // val debug = IO(Output(Vec(cfg.registerNum, UInt(cfg.xlen.W))))
 
-  val regFile = if (cfg.formal) { RegInit(rvspeccore.checker.ArbitraryRegFile.gen(cfg.xlen)) }
-  else { Reg(Vec(cfg.registerNum, UInt(cfg.xlen.W))) }
+  // val regFile = if (cfg.formal) { RegInit(rvspeccore.checker.ArbitraryRegFile.gen(cfg.xlen)) }
+  // else { Reg(Vec(cfg.registerNum, UInt(cfg.xlen.W))) }
+  //
+  // when(wbuIn.wEn) {
+  //   regFile(wbuIn.wAddr) := wbuIn.wData
+  // }
+  // regFile(0) := 0.U
+  // iduIn.rData(0) := regFile(iduIn.rAddr(0))
+  // iduIn.rData(1) := regFile(iduIn.rAddr(1))
+  //
+  // if (cfg.formal) {
+  //   require(cfg.registerNum == 32)
+  //   rvspeccore.checker.ConnectHelper.setRegSource(regFile)
+  // }
+  //
+  // if (cfg.isDebug) {
+  //   debug.get := regFile
+  // }
+  // // debug := regFile
 
-  when(wbuIn.wEn) {
-    regFile(wbuIn.wAddr) := wbuIn.wData
+  trait HasReadWrite {
+    def read(idx:  UInt):             UInt
+    def readAll:                      Seq[UInt]
+    def write(idx: UInt, data: UInt): Unit
   }
-  regFile(0) := 0.U
-  iduIn.rData(0) := regFile(iduIn.rAddr(0))
-  iduIn.rData(1) := regFile(iduIn.rAddr(1))
+  class RegReg extends HasReadWrite {
+    val rf = if (cfg.formal) { RegInit(rvspeccore.checker.ArbitraryRegFile.gen(cfg.xlen)) }
+    else { Reg(Vec(cfg.registerNum, UInt(cfg.xlen.W))) }
+    def read(addr:  UInt): UInt = Mux(addr === 0.U, 0.U, rf(addr))
+    def readAll:           Seq[UInt] = Seq(0.U) ++ (1 until cfg.registerNum).map { idx => rf(idx) }
+    def write(addr: UInt, data: UInt) = { rf(addr) := data }
+  }
+  class RegMem extends HasReadWrite {
+    val rf = Mem(cfg.registerNum, UInt(cfg.xlen.W))
+    def read(addr:  UInt): UInt = Mux(addr === 0.U, 0.U, rf(addr))
+    def readAll:           Seq[UInt] = Seq(0.U) ++ (1 until cfg.registerNum).map { idx => rf(idx) }
+    def write(addr: UInt, data: UInt) = { rf(addr) := data }
+  }
+
+  val regFile = if (cfg.formal) { new RegReg } else { new RegMem }
+  when(wbuIn.wEn) {
+    regFile.write(wbuIn.wAddr, wbuIn.wData)
+  }
+  iduIn.rData(0) := regFile.read(iduIn.rAddr(0))
+  iduIn.rData(1) := regFile.read(iduIn.rAddr(1))
 
   if (cfg.formal) {
     require(cfg.registerNum == 32)
-    rvspeccore.checker.ConnectHelper.setRegSource(regFile)
+    val rfFormal = VecInit(regFile.readAll)
+    rvspeccore.checker.ConnectHelper.setRegSource(rfFormal)
   }
 
   if (cfg.isDebug) {
-    debug.get := regFile
+    val rfDebug = VecInit(regFile.readAll)
+    debug.get := rfDebug
   }
-  // debug := regFile
-
-  // val regFile = Mem(cfg.registerNum, UInt(cfg.xlen.W))
-  // when(wbuIn.wEn && (wbuIn.wAddr =/= 0.U)) {
-  //   regFile.write(wbuIn.wAddr, wbuIn.wData)
-  // }
-  // iduIn.rData(0) := regFile.read(iduIn.rAddr(0))
-  // iduIn.rData(1) := regFile.read(iduIn.rAddr(1))
-  // when (iduIn.rAddr(0) === 0.U) {
-  //   iduIn.rData(0) := 0.U
-  // }
-  // when (iduIn.rAddr(1) === 0.U) {
-  //   iduIn.rData(1) := 0.U
-  // }
 }
