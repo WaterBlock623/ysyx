a90c7595591bceb07fbdc870d132f3165c340aae
tracer-ysyx <tracer@ysyx.org>
2026-05-04 19:28:51 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:28:51 up  6:54,  1 user,  load average: 0.13, 0.57, 0.97

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -11,6 +11,7 @@ AM_SRCS := riscv/npc/start.S \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 # CFLAGS += -falign-functions=8 -falign-loops=8
+CFLAGS += $(if $(AFDO),-fauto-profile=$(AFDO),)
 LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDSCRIPTS_MEM += $(AM_HOME)/scripts/linker-mem.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
