fab3170a6e95a10a01c7b6043b68d7163d5c27b8
tracer-ysyx <tracer@ysyx.org>
2026-05-15 11:24:50 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:24:50 up 2 days,  1:54,  1 user,  load average: 2.84, 2.46, 2.38

--- npc/scripts/iverilog.mk
+++ npc/scripts/iverilog.mk
@@ -2,7 +2,7 @@ IVERILOG_HEX = $(BUILD_DIR)/$(notdir $(basename $(IMG)))_iverilog.hex
 # IVERILOG_VSRCS = $(BUILD_DIR)/iverilog-vsrc/*.sv $(NPC_HOME)/iverilog/iverilog_top.sv
 IVERILOG_VSRCS = $(NPC_HOME)/build/ysyx_26010008.sv
 
-$(IVERILOG_HEX):
+$(IVERILOG_HEX): $(IMG)
 	mkdir -p $(dir $@)
 	# objcopy -I binary -O verilog $(IMG) $@
 	hexdump -v -e '1/4 "%08x" "\n"' $(IMG) > $@
