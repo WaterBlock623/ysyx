34f1f3978a1ed460ddcbe83980b145851a8f58fd
tracer-ysyx <tracer@ysyx.org>
2026-05-15 13:07:18 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:07:18 up  1:05,  1 user,  load average: 0.14, 0.17, 0.33

--- npc/scripts/iverilog.mk
+++ npc/scripts/iverilog.mk
@@ -1,17 +1,21 @@
 IVERILOG_HEX = $(BUILD_DIR)/$(notdir $(basename $(IMG)))_iverilog.hex
 IVERILOG_VSRCS += $(NPC_HOME)/iverilog/iverilog_top.sv
-IVERILOG_VSRCS += $(NPC_HOME)/build/ysyx_26010008.sv
+IVERILOG_VSRCS += $(BUILD_DIR)/iverilog/*.sv
 
 $(IVERILOG_HEX): $(IMG)
 	mkdir -p $(dir $@)
 	# objcopy -I binary -O verilog $(IMG) $@
 	hexdump -v -e '1/4 "%08x" "\n"' $(IMG) > $@
 
-sim-iverilog: $(IVERILOG_HEX)
+sim-iverilog-base: $(IVERILOG_HEX)
 	$(MAKE) -C $(NPC_HOME) verilog ARCH=iverilog
 	iverilog -g2012 -s iverilog_top -o $(BUILD_DIR)/iverilog.vvp \
 		-D IVERILOG_HEX_PATH="\"$(IVERILOG_HEX)\"" \
 		$(IVERILOG_VSRCS)
 	vvp $(BUILD_DIR)/iverilog.vvp
 
-sim-iverilog-netlist: $(IVERILOG_HEX)
+sim-iverilog: IVERILOG_VSRCS += $(NPC_HOME)/build/ysyx_26010008.sv
+sim-iverilog: sim-iverilog-base
+
+sim-iverilog-netlist: IVERILOG_VSRCS += $(NETLIST) $(CELLS)
+sim-iverilog-netlist: sim-iverilog-base
