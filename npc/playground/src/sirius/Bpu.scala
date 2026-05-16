88c42aeac1a20b79952a15d2824149f829509fad
tracer-ysyx <tracer@ysyx.org>
2026-05-16 10:57:53 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:57:53 up  2:23,  1 user,  load average: 6.22, 1.79, 1.01

--- npc/playground/src/sirius/Bpu.scala
+++ npc/playground/src/sirius/Bpu.scala
@@ -30,9 +30,10 @@ class Btb(
   val tags =
     if (cfg.iverilog) RegInit(0.U.asTypeOf(Vec(lineNum, UInt(tagWidth.W))))
     else Reg(Vec(lineNum, UInt(tagWidth.W)))
-  val targets = 
-    if (cfg.iverilog) RegInit(0.U.asTypeOf(Vec(lineNum, UInt(targetWidth.W))))
-    else Reg(Vec(lineNum, UInt(targetWidth.W)))
+  // val targets = 
+  //   if (cfg.iverilog) RegInit(0.U.asTypeOf(Vec(lineNum, UInt(targetWidth.W))))
+  //   else Reg(Vec(lineNum, UInt(targetWidth.W)))
+  val targets = Reg(Vec(lineNum, UInt(targetWidth.W)))
 
   def significantPc(pc: UInt) = pc >> trivialBits
   def hashTagIndex(pc: UInt) = {
