fd4f36d55bcbc71a6e4b36b4052edf3db6b56d64
tracer-ysyx <tracer@ysyx.org>
2026-05-18 14:29:52 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:29:52 up  2:24,  1 user,  load average: 0.20, 0.59, 0.68

--- nemu/src/monitor/gdb/gdb.c
+++ nemu/src/monitor/gdb/gdb.c
@@ -12,11 +12,12 @@ extern bool g_cpu_stop_flag;
 
 gdb_action_t emu_cont(void *args) {
   cpu_exec(-1);
-  if (nemu_state.state == NEMU_STOP || nemu_state.state == NEMU_ABORT) {
-    return ACT_RESUME;
-  } else {
-    return ACT_SHUTDOWN;
-  }
+  // if (nemu_state.state == NEMU_STOP || nemu_state.state == NEMU_ABORT) {
+  //   return ACT_RESUME;
+  // } else {
+  //   return ACT_SHUTDOWN;
+  // }
+  return ACT_RESUME;
 }
 
 gdb_action_t emu_stepi(void *args) {
