6fc32aba604aa67fea462a1615bdade822085c76
tracer-ysyx <tracer@ysyx.org>
2026-05-19 23:46:58 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:46:58 up 12:37,  1 user,  load average: 0.07, 0.02, 0.00

--- nemu/scripts/init.mk
+++ nemu/scripts/init.mk
@@ -1,3 +1,3 @@
-ifeq ($(wildcard $(NEMU_HOME)/tools/mini-gdbstub),)
+ifeq ($(wildcard $(NEMU_HOME)/tools/mini-gdbstub/.git),)
   $(shell git submodule update --init $(NEMU_HOME)/tools/mini-gdbstub)
 endif
