7f64f2661ae266afbd4ccecd7a149655cd219b97
Yan Chenrui <17048545@qq.com>
2026-05-20 14:42:00 +0800

    Fix makefile format

--- npc/scripts/init.mk
+++ npc/scripts/init.mk
@@ -1,16 +1,16 @@
 ifeq ($(wildcard $(NPC_HOME)/ChiRVFormal/.git),)
-  $(info Init ChiRVFormal...)
-  $(shell wget -q -O - https://github.com/sbt/sbt/releases/download/v1.12.11/sbt-1.12.11.tgz | tar -zxvf -)
-  $(shell git submodule update --init $(NPC_HOME)/ChiRVFormal)
-  $(shell cd $(NPC_HOME)/ChiRVFormal; ../sbt/bin/sbt publishLocal -DHashId=true -DChiselVersion=7.11.0 -DScalaVersion=2.13.18 > /dev/null 2>&1)
+$(info Init ChiRVFormal...)
+$(shell wget -q -O - https\://github.com/sbt/sbt/releases/download/v1.12.11/sbt-1.12.11.tgz | tar -zxvf -)
+$(shell git submodule update --init $(NPC_HOME)/ChiRVFormal)
+$(shell cd $(NPC_HOME)/ChiRVFormal; ../sbt/bin/sbt publishLocal -DHashId=true -DChiselVersion=7.11.0 -DScalaVersion=2.13.18 > /dev/null 2>&1)
 endif
 
 ifeq ($(wildcard $(NPC_HOME)/rvdecoderdb/.git),)
-  $(info Init rvdecoderdb...)
-  $(shell git submodule update --init $(NPC_HOME)/rvdecoderdb)
+$(info Init rvdecoderdb...)
+$(shell git submodule update --init $(NPC_HOME)/rvdecoderdb)
 endif
 
 ifeq ($(wildcard $(NPC_HOME)/riscv-opcodes/.git),)
-  $(info Init riscv-opcodes...)
-  $(shell git submodule update --init $(NPC_HOME)/riscv-opcodes)
+$(info Init riscv-opcodes...)
+$(shell git submodule update --init $(NPC_HOME)/riscv-opcodes)
 endif
