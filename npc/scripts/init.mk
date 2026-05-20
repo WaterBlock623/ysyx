e0adb308a185b848e49583076ff0614b32d523ec
Yan Chenrui <17048545@qq.com>
2026-05-20 16:46:53 +0800

    Track iverilog-top Fix wget arguments Disable UART16550 $write

--- npc/scripts/init.mk
+++ npc/scripts/init.mk
@@ -22,6 +22,7 @@ endif
 ifeq ($(shell command -v espresso),)
 $(info Init espresso...)
 $(shell mkdir -p $(NPC_HOME)/espresso)
-$(shell wget https\://github.com/chipsalliance/espresso/releases/download/v2.4/x86_64-linux-gnu-espresso -o $(NPC_HOME)/espresso > /dev/null 2>&1)
+$(shell wget https\://github.com/chipsalliance/espresso/releases/download/v2.4/x86_64-linux-gnu-espresso -O $(NPC_HOME)/espresso > /dev/null 2>&1)
 $(shell export PATH=$(NPC_HOME)/espresso:$$PATH)
 endif
+
