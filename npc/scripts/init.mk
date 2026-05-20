38d38e20cc368cfc2282dc04091b0f933f1b903e
Yan Chenrui <17048545@qq.com>
2026-05-20 14:51:31 +0800

    Disable tar stdout

--- npc/scripts/init.mk
+++ npc/scripts/init.mk
@@ -1,6 +1,6 @@
 ifeq ($(wildcard $(NPC_HOME)/ChiRVFormal/.git),)
 $(info Init ChiRVFormal...)
-$(shell wget -q -O - https\://github.com/sbt/sbt/releases/download/v1.12.11/sbt-1.12.11.tgz | tar -zxvf -)
+$(shell wget -q -O - https\://github.com/sbt/sbt/releases/download/v1.12.11/sbt-1.12.11.tgz | tar -zxvf - > /dev/null 2>&1)
 $(shell git submodule update --init $(NPC_HOME)/ChiRVFormal)
 $(shell cd $(NPC_HOME)/ChiRVFormal; ../sbt/bin/sbt publishLocal -DHashId=true -DChiselVersion=7.11.0 -DScalaVersion=2.13.18 > /dev/null 2>&1)
 endif
