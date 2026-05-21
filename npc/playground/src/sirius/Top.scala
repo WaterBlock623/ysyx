3c1261fec6d1c10ab902857b1028cd3883637a76
tracer-ysyx <tracer@ysyx.org>
2026-05-21 15:21:48 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:21:48 up  5:37,  1 user,  load average: 6.34, 2.78, 1.41

--- npc/playground/src/sirius/Top.scala
+++ npc/playground/src/sirius/Top.scala
@@ -46,10 +46,10 @@ class BasicCore(
   if (!cfg.formal) {
     val bpu = Module(
       new Bpu(
-        btbIndexWidth = 3,
+        btbIndexWidth = 2,
         btbTagWidth = 4,
-        btbTargetWidth = 14,
-        phtIndexWidth = 5,
+        btbTargetWidth = 10,
+        phtIndexWidth = 4,
         phtCounterWidth = 2
       )
     )
@@ -111,16 +111,16 @@ class BasicCore(
       }
     }
     val stageRds = Seq(
-      // StageRd(
-      //   exu.in.valid,
-      //   exu.in.bits.ctrl.wbuCtrl.isWriteBackReg,
-      //   exu.in.bits.iduPayload.idu.wAddr,
-      //   Seq(
-      //     (exu.out.valid &&
-      //       (exu.in.bits.ctrl.wbuCtrl.writeBackSel === WriteBackSelEnum.alu.asUInt)) ->
-      //       exu.out.bits.exuPayload.exu.aluOut
-      //   )
-      // ),
+      StageRd(
+        exu.in.valid,
+        exu.in.bits.ctrl.wbuCtrl.isWriteBackReg,
+        exu.in.bits.iduPayload.idu.wAddr,
+        Seq(
+          (exu.out.valid &&
+            (exu.in.bits.ctrl.wbuCtrl.writeBackSel === WriteBackSelEnum.alu.asUInt)) ->
+            exu.out.bits.exuPayload.exu.aluOut
+        )
+      ),
       StageRd(
         lsu.in.valid,
         lsu.in.bits.ctrl.wbuCtrl.isWriteBackReg,
