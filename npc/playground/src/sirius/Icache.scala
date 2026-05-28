d96b6a59773bab56ceb30555eb871f329582ed6a
tracer-ysyx <tracer@ysyx.org>
2026-05-28 15:47:13 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:47:13 up  5:07,  1 user,  load average: 0.81, 0.54, 0.48

--- npc/playground/src/sirius/Icache.scala
+++ npc/playground/src/sirius/Icache.scala
@@ -315,40 +315,47 @@ class SimpleIcache(
   val sInvalid :: sValid :: sAbort :: Nil = Enum(3)
   val dataState = RegInit(sInvalid)
   val newData =
-    ((state === sReadCache) && hit && inWhiteList) || ((state === sFirstResp) && io.mem.r.fire)
+    ((state === sReadCache) && hit) || ((state === sFirstResp) && io.mem.r.fire)
   val dataReg = RegEnable(Mux(state === sReadCache, hitData, io.mem.r.bits.data), newData)
 
-  switch(dataState) {
-    is(sInvalid) {
-      when(io.cached.abort) {
-        dataState := sAbort
-      }.elsewhen(newData) {
-        dataState := sValid
-      }
-    }
-    is(sValid) {
-      when(io.cached.abort) {
-        dataState := sAbort
-      }.elsewhen(io.cached.r.fire) {
-        dataState := sInvalid
-      }
-    }
+  // switch(dataState) {
+  //   is(sInvalid) {
+  //     when(io.cached.abort) {
+  //       dataState := sAbort
+  //     }.elsewhen(newData) {
+  //       dataState := sValid
+  //     }
+  //   }
+  //   is(sValid) {
+  //     when(io.cached.abort) {
+  //       dataState := sAbort
+  //     }.elsewhen(io.cached.r.fire) {
+  //       dataState := sInvalid
+  //     }
+  //   }
+  // }
+  when(io.cached.abort) {
+    dataState := sAbort
   }
 
   switch(state) {
     is(sReadCache) {
-      dataState := sInvalid
       when(!(hit && inWhiteList)) {
         state := sReqMem
+        dataState := sInvalid
       }.otherwise {
         addrReg := addrReg + 1.U
+        dataState := sValid
       }
     }
     is(sReqMem) {
       when(io.mem.ar.fire) { state := sFirstResp }
     }
     is(sFirstResp) {
-      when(io.mem.r.fire) { state := Mux(io.mem.r.bits.last, sWaitConsume, sFillCache) }
+      when(io.mem.r.fire) {
+        state := Mux(io.mem.r.bits.last, sWaitConsume, sFillCache)
+        when(dataState =/= sAbort) { dataState := sValid }
+      }
     }
     is(sFillCache) {
       when(io.mem.r.fire) { state := sWaitConsume }
