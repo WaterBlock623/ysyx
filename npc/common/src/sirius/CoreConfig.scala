6540ffbe5432e4113d17083e05082ad8584cba73
tracer-ysyx <tracer@ysyx.org>
2026-05-28 10:50:34 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:50:34 up 15 min,  1 user,  load average: 0.39, 0.54, 0.56

--- npc/common/src/sirius/CoreConfig.scala
+++ npc/common/src/sirius/CoreConfig.scala
@@ -11,7 +11,8 @@ case class CoreConfig(
   val isDebug:      Boolean = true,
   val perf:         Boolean = true,
   val ysyxsoc:      Boolean = true,
-  val iverilog:      Boolean = false,
+  val iverilog:     Boolean = false,
+  val formal:       Boolean = false,
   val modulePrefix: Option[String] = None,
 
   // rvdecoderdb
@@ -35,7 +36,7 @@ case class CoreConfig(
   val memoryAddrWidth:     Int = 32,
   val pcInit:              BigInt = 0x30000000,
   val pipeline:            Boolean = true,
-  val formal:              Boolean = false,
+  val predTargetWidth:     Int = 15,
 
   // CsrID
   val mvendorid: Int = 0x79737978,
@@ -44,6 +45,8 @@ case class CoreConfig(
   val mxlen:       Int = xlen
   val registerNum: Int = 1 << registerAddrWidth
   require(memoryAddrWidth <= 32)
+
+  def hasC: Boolean = extensions().contains(ExtTypeEnum.C)
 
   // 根据配置映射解码Pattern和Field
   def fieldMap: CfgMap[
