6ff26d385eb8ab55e73d96c7e07974ffdc642bbf
tracer-ysyx <tracer@ysyx.org>
2026-06-01 21:39:51 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:39:51 up  6:18,  1 user,  load average: 0.20, 0.75, 1.01

--- npc/playground/src/sirius/Icache.scala
+++ npc/playground/src/sirius/Icache.scala
@@ -314,6 +314,7 @@ class SimpleIcache(
   val sReadCache :: sReqMem :: sFirstResp :: sFillCache :: Nil = Enum(4)
   val state = RegInit(sReadCache)
   val abortReg = RegInit(false.B)
+  val abort = abortReg || io.cached.abort
 
   when(io.mem.r.fire && io.mem.r.bits.last) {
     abortReg := false.B
@@ -340,7 +341,7 @@ class SimpleIcache(
       when(io.mem.r.fire) {
         when(io.mem.r.bits.last) {
           state := sReadCache
-          addrReg := Mux(abortReg, newAddr, staticNextAddrReg)
+          addrReg := Mux(abort, newAddr, staticNextAddrReg)
         }.otherwise {
           state := sFillCache
         }
@@ -349,7 +350,7 @@ class SimpleIcache(
     is(sFillCache) {
       when(io.mem.r.fire) {
         state := sReadCache
-        addrReg := Mux(abortReg, newAddr, staticNextAddrReg)
+        addrReg := Mux(abort, newAddr, staticNextAddrReg)
       }
     }
   }
