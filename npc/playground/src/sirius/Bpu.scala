d1aa6a1ef3dae829e5073bc9ea81f34f702c0a21
tracer-ysyx <tracer@ysyx.org>
2026-06-03 16:24:02 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:24:02 up  5:39,  1 user,  load average: 1.36, 1.78, 1.32

--- npc/playground/src/sirius/Bpu.scala
+++ npc/playground/src/sirius/Bpu.scala
@@ -27,9 +27,10 @@ class Btb(
   val trivialBits = if (cfg.extensions().contains(ExtTypeEnum.C)) 1 else 2
 
   // val tags = Reg(Vec(lineNum, UInt(tagWidth.W)))
-  val tags =
-    if (cfg.iverilog) RegInit(0.U.asTypeOf(Vec(lineNum, UInt(tagWidth.W))))
-    else Reg(Vec(lineNum, UInt(tagWidth.W)))
+  // val tags =
+  //   if (cfg.iverilog) RegInit(0.U.asTypeOf(Vec(lineNum, UInt(tagWidth.W))))
+  //   else Reg(Vec(lineNum, UInt(tagWidth.W)))
+  val tags = RegInit(0.U.asTypeOf(Vec(lineNum, UInt(tagWidth.W))))
   // val targets = 
   //   if (cfg.iverilog) RegInit(0.U.asTypeOf(Vec(lineNum, UInt(targetWidth.W))))
   //   else Reg(Vec(lineNum, UInt(targetWidth.W)))
