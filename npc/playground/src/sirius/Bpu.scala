9c2eaa25794e74594403c88408f447528081222f
tracer-ysyx <tracer@ysyx.org>
2026-06-03 17:23:29 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:23:29 up  6:40,  1 user,  load average: 6.37, 2.88, 1.83

--- npc/playground/src/sirius/Bpu.scala
+++ npc/playground/src/sirius/Bpu.scala
@@ -79,11 +79,11 @@ class Pht(
   })
 
   val cntNum = 1 << indexWidth
-  val cnts = Reg(Vec(cntNum, UInt(2.W)))
+  // val cnts = Reg(Vec(cntNum, UInt(2.W)))
   // val cnts =
   //   if (cfg.iverilog) RegInit(0.U.asTypeOf(Vec(cntNum, UInt(2.W))))
   //   else Reg(Vec(cntNum, UInt(2.W)))
-  // val cnts = RegInit(0.U.asTypeOf(Vec(cntNum, UInt(2.W))))
+  val cnts = RegInit(0.U.asTypeOf(Vec(cntNum, UInt(2.W))))
 
   def significantPc(pc: UInt): UInt = if (cfg.extensions().contains(ExtTypeEnum.C)) { pc >> 1 }
   else { pc >> 2 }
