9d1fdc335ad6e93cc4ce96b8a00faae307be131e
tracer-ysyx <tracer@ysyx.org>
2026-06-03 16:29:15 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:29:15 up  5:44,  1 user,  load average: 4.50, 3.95, 2.38

--- npc/playground/src/sirius/Icache.scala
+++ npc/playground/src/sirius/Icache.scala
@@ -325,7 +325,7 @@ class SimpleIcache(
 
   when(io.mem.r.fire && io.mem.r.bits.last) {
     disableOverrideReg := false.B
-  }.elsewhen((state === sReqMem || (state === sFirstResp && !io.mem.r.valid)) && io.cached.abort) {
+  }.elsewhen((state === sReqMem || (state === sFirstResp && !io.mem.r.fire)) && io.cached.abort) {
     disableOverrideReg := true.B
   }
 
