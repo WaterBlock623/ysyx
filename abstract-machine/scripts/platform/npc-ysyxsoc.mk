ae37cb750049e7efa2d9298d6f6d39c01577051f
Yan Chenrui <17048545@qq.com>
2026-06-05 17:25:27 +0800

    Add debug info for CI

--- abstract-machine/scripts/platform/npc-ysyxsoc.mk
+++ abstract-machine/scripts/platform/npc-ysyxsoc.mk
@@ -33,6 +33,8 @@ insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
 
 image: image-dep
+	$(CC) --version
+	$(OBJDUMP) -h $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S -O binary $(IMAGE).elf $(IMAGE).bin
