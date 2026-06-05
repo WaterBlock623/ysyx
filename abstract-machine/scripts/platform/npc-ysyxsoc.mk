1ca0b570e94d40919049749bcef33ffd07d130c5
tracer-ysyx <tracer@ysyx.org>
2026-06-05 22:39:44 +0800

    > compile NEMU ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:39:44 up 1 day, 10:39,  1 user,  load average: 3.56, 2.68, 2.41

--- abstract-machine/scripts/platform/npc-ysyxsoc.mk
+++ abstract-machine/scripts/platform/npc-ysyxsoc.mk
@@ -40,7 +40,16 @@ image: image-dep
 	cat $(IMAGE).map
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S -O binary $(IMAGE).elf $(IMAGE).bin
-	base64 $(IMAGE).bin
+	@echo -e "\n\nMD5 ELF\n\n"
+	md5sum $(IMAGE).elf
+	@echo -e "\n\nBASE64 ELF\n\n"
+	base64 -w0 $(IMAGE).elf > $(IMAGE).elf.base64
+	cat $(IMAGE).elf.base64
+	@echo -e "\n\nMD5 BIN\n\n"
+	md5sum $(IMAGE).bin
+	@echo -e "\n\nBASE64 BIN\n\n"
+	base64 -w0 $(IMAGE).bin > $(IMAGE).bin.base64
+	cat $(IMAGE).bin.base64
 
 run: insert-arg
 	$(MAKE) -C $(NPC_HOME) run 
