d56efd8d09efc5c644a027b16d532492ac5ed7ef
Yan Chenrui <17048545@qq.com>
2026-06-06 00:01:17 +0800

    Remove some debug info

--- abstract-machine/scripts/platform/npc-ysyxsoc.mk
+++ abstract-machine/scripts/platform/npc-ysyxsoc.mk
@@ -40,16 +40,11 @@ image: image-dep
 	cat $(IMAGE).map
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S -O binary $(IMAGE).elf $(IMAGE).bin
-	@echo -e "\n\nMD5 ELF\n\n"
+	@echo -e "\nMD5 ELF\n"
 	md5sum $(IMAGE).elf
-	@echo -e "\n\nBASE64 ELF\n\n"
-	base64 -w0 $(IMAGE).elf > $(IMAGE).elf.base64
-	cat $(IMAGE).elf.base64
-	@echo -e "\n\nMD5 BIN\n\n"
+	@echo -e "\nMD5 BIN\n"
 	md5sum $(IMAGE).bin
-	@echo -e "\n\nBASE64 BIN\n\n"
-	base64 -w0 $(IMAGE).bin > $(IMAGE).bin.base64
-	cat $(IMAGE).bin.base64
+	@echo -e "\n"
 
 run: insert-arg
 	$(MAKE) -C $(NPC_HOME) run 
