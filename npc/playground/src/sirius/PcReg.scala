fc50e8a946741c45a09bb93121de9fea81ec80bd
tracer-ysyx <tracer@ysyx.org>
2026-06-08 12:12:23 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:12:23 up  1:39,  1 user,  load average: 4.95, 2.83, 1.62

--- npc/playground/src/sirius/PcReg.scala
+++ npc/playground/src/sirius/PcReg.scala
@@ -2,30 +2,30 @@ package sirius
 
 import chisel3._
 
-class PcReg(
-  implicit private val cfg: CoreConfig)
-    extends Module {
-  val ifuIn = IO(Flipped(new IfuToPcRegIO))
-  val lsuIn = IO(Flipped(new LsuToPcRegIO))
-  val wbuIn = IO(Flipped(new WbuToPcRegIO))
-  val debug = Option.when(cfg.isDebug)(IO(new Bundle {
-    val pc = Output(UInt(cfg.xlen.W))
-  }))
-
-  val pcReg = RegInit(cfg.pcInit.U(cfg.xlen.W))
-  when (ifuIn.update) {
-    pcReg := ifuIn.nextPc
-  }
-  when (wbuIn.isJump || lsuIn.isJump) {
-    assert(!(wbuIn.isJump && lsuIn.isJump))
-    pcReg := Mux(wbuIn.isJump, wbuIn.target, lsuIn.target)
-  }
-  ifuIn.pc := pcReg
-
-  if (cfg.isDebug) {
-    debug.get.pc := pcReg
-  }
-}
+// class PcReg(
+//   implicit private val cfg: CoreConfig)
+//     extends Module {
+//   val ifuIn = IO(Flipped(new IfuToPcRegIO))
+//   val lsuIn = IO(Flipped(new LsuToPcRegIO))
+//   val wbuIn = IO(Flipped(new WbuToPcRegIO))
+//   val debug = Option.when(cfg.isDebug)(IO(new Bundle {
+//     val pc = Output(UInt(cfg.xlen.W))
+//   }))
+//
+//   val pcReg = RegInit(cfg.pcInit.U(cfg.xlen.W))
+//   when (ifuIn.update) {
+//     pcReg := ifuIn.nextPc
+//   }
+//   when (wbuIn.isJump || lsuIn.isJump) {
+//     assert(!(wbuIn.isJump && lsuIn.isJump))
+//     pcReg := Mux(wbuIn.isJump, wbuIn.target, lsuIn.target)
+//   }
+//   ifuIn.pc := pcReg
+//
+//   if (cfg.isDebug) {
+//     debug.get.pc := pcReg
+//   }
+// }
 
 class GlobalPcHi(implicit private val cfg: CoreConfig) extends Module {
   val trivialBits = if (cfg.hasC) 1 else 2
