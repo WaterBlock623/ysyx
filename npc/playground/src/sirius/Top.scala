b092050c42add3b0bf08b38a5ff5ef6bd84e6d6e
tracer-ysyx <tracer@ysyx.org>
2026-06-13 00:58:59 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  00:58:59 up  2:32,  1 user,  load average: 0.55, 1.06, 1.24

--- npc/playground/src/sirius/Top.scala
+++ npc/playground/src/sirius/Top.scala
@@ -46,7 +46,7 @@ class BasicCore(
     val bpu = Module(
       new Bpu(
         btbIndexWidth = 3,
-        btbTagWidth = 3,
+        btbTagWidth = 4,
         btbTargetWidth = cfg.predTargetWidth,
         phtIndexWidth = 3,
         phtCounterWidth = 2
