e107cc6dbfb7c92b0775c7676f7f6058afdb877b
tracer-ysyx <tracer@ysyx.org>
2026-06-13 00:51:01 +0800

    > generate verilog ysyx_26010008 严晨瑞 Linux DESKTOP-L34PO7D 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  00:51:01 up  2:24,  1 user,  load average: 1.09, 1.96, 1.44

--- npc/playground/src/sirius/Xbar.scala
+++ npc/playground/src/sirius/Xbar.scala
@@ -231,7 +231,7 @@ class SimpleXbar extends Module {
 
   io.out(1).aw :<= 0.U.asTypeOf(chiselTypeOf(io.out(1).aw))
   io.out(1).w :<= 0.U.asTypeOf(chiselTypeOf(io.out(1).w))
-  0.U.asTypeOf(chiselTypeOf(io.in(1).b)) :>= io.in(1).b
+  io.in(1).b :<= 0.U.asTypeOf(chiselTypeOf(io.in(1).b))
   // val wAllowHandshake = RegInit(false.B)
   // val wOutSel = RegInit(false.B)
   //
